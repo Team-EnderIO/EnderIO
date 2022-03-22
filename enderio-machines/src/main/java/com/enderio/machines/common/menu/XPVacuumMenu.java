9326a32f878789bc7b4fb95014d247d490fa0ec2
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2022-03-22 22:03:10 +0100

    fix spacing

--- enderio-machines/src/main/java/com/enderio/machines/common/menu/XPVacuumMenu.java
+++ enderio-machines/src/main/java/com/enderio/machines/common/menu/XPVacuumMenu.java
@@ -13,16 +13,16 @@ import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
 public class XPVacuumMenu extends MachineMenu<XPVacuumBlockEntity>{
-
-	public XPVacuumMenu(XPVacuumBlockEntity blockEntity, Inventory inventory, int pContainerId) {
-		super(blockEntity, inventory, MachineMenus.XP_VACUUM.get(), pContainerId);
-		if (blockEntity != null) {
-			
-		}
-		addInventorySlots(8,70);
-	}
-	
-	public static XPVacuumMenu factory(@Nullable MenuType<XPVacuumMenu> pMenuType, int pContainerId, Inventory inventory, FriendlyByteBuf buf) {
+    
+    public XPVacuumMenu(XPVacuumBlockEntity blockEntity, Inventory inventory, int pContainerId) {
+        super(blockEntity, inventory, MachineMenus.XP_VACUUM.get(), pContainerId);
+        if (blockEntity != null) {
+            
+        }
+        addInventorySlots(8,70);
+    }
+    
+    public static XPVacuumMenu factory(@Nullable MenuType<XPVacuumMenu> pMenuType, int pContainerId, Inventory inventory, FriendlyByteBuf buf) {
         BlockEntity entity = inventory.player.level.getBlockEntity(buf.readBlockPos());
         if (entity instanceof XPVacuumBlockEntity castBlockEntity)
             return new XPVacuumMenu(castBlockEntity, inventory, pContainerId);
