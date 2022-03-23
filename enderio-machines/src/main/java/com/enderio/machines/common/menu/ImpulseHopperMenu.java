37c6bc1eddffe3130e1d71ec1add9cf95698401d
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2022-03-23 13:56:09 +0100

    fix dragging

--- enderio-machines/src/main/java/com/enderio/machines/common/menu/ImpulseHopperMenu.java
+++ enderio-machines/src/main/java/com/enderio/machines/common/menu/ImpulseHopperMenu.java
@@ -7,7 +7,11 @@ import com.enderio.machines.common.init.MachineMenus;
 
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ClickType;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
 public class ImpulseHopperMenu extends MachineMenu<ImpulseHopperBlockEntity>{
@@ -36,5 +40,39 @@ public class ImpulseHopperMenu extends MachineMenu<ImpulseHopperBlockEntity>{
         LogManager.getLogger().warn("couldn't find BlockEntity");
         return new ImpulseHopperMenu(null, inventory, pContainerId);
     }
+	
+	//Fix dragging items still being removed when the slot is a ghost slot.
+	@Override
+	public void clicked(int p_150400_, int button, ClickType clicktype, Player player) {
+        int h = getQuickcraftHeader(button);
+        if (h == 2 && this.quickcraftSlots.size() > 1) {
+            ItemStack itemstack3 = this.getCarried().copy();
+            int j1 = this.getCarried().getCount();
 
+            for(Slot slot1 : this.quickcraftSlots) {
+               ItemStack itemstack1 = this.getCarried();
+               if (slot1 != null && canItemQuickReplace(slot1, itemstack1, true) && slot1.mayPlace(itemstack1) && (getQuickcraftType(button) == 2 || itemstack1.getCount() >= this.quickcraftSlots.size()) && this.canDragTo(slot1)) {
+                  ItemStack itemstack2 = itemstack3.copy();
+                  int j = slot1.hasItem() ? slot1.getItem().getCount() : 0;
+                  getQuickCraftSlotCount(this.quickcraftSlots, getQuickcraftType(button), itemstack2, j);
+                  int k = Math.min(itemstack2.getMaxStackSize(), slot1.getMaxStackSize(itemstack2));
+                  if (itemstack2.getCount() > k) {
+                     itemstack2.setCount(k);
+                  }
+
+                  if (slot1 instanceof GhostSlot) {
+                      //TODO maybe a different check?
+                  } else {
+                      j1 -= itemstack2.getCount() - j;
+                  }
+                  slot1.set(itemstack2);
+               }
+            }
+
+            itemstack3.setCount(j1);
+            this.setCarried(itemstack3);
+            return;
+        }
+	    super.clicked(p_150400_, button, clicktype, player);
+	}
 }
