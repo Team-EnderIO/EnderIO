40518ddca7a419d7be683c0fa128720687154380
Rover656 <20544390+Rover656@users.noreply.github.com>
2022-06-01 23:33:15 +0100

    This should work.

--- buildSrc/src/main/groovy/com/enderio/CombineJars.groovy
+++ buildSrc/src/main/groovy/com/enderio/CombineJars.groovy
@@ -3,6 +3,7 @@ package com.enderio
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
 import org.gradle.api.Project
+import org.gradle.api.file.CopySpec
 import org.gradle.api.tasks.util.PatternFilterable
 
 // Credit: Mekanism for the base concept.
@@ -15,9 +16,8 @@ class CombineJars {
         toExclude.add('.cache/cache')
 
         // Exclude duplicated data
-        def resourcesDir = project.rootProject.sourceSets.main.output.resourcesDir
-        int baseLength = "$resourcesDir".length()
-        project.fileTree(dir: "$resourcesDir/data/").each {
+        int baseLength = "$project.rootProject.buildDir/generated/".length()
+        project.fileTree(dir: "$project.rootProject.buildDir/generated/data/").each {
             File file -> toExclude.add(file.getPath().substring(baseLength))
         }
 
@@ -33,38 +33,55 @@ class CombineJars {
      * Merge mod resources together.
      */
     static void mergeResources(Project rootProject, List<Project> toMerge) {
+        println("[Mod Merger] Merging mod resources.")
+
         // Create resource directories
-        def resourcesDir = rootProject.sourceSets.main.output.resourcesDir
-        rootProject.mkdir("$resourcesDir/META-INF")
+        def generatedDir = "$rootProject.buildDir/generated"
+        rootProject.mkdir("$generatedDir/META-INF")
 
         // Delete data resources to prevent pollution
-        rootProject.file("$resourcesDir/data").deleteDir()
-        rootProject.mkdir("$resourcesDir/data")
+        rootProject.file("$generatedDir/data").deleteDir()
+        rootProject.mkdir("$generatedDir/data")
 
-        new File(resourcesDir, "META-INF/mods.toml") << mergeManifests(toMerge)
-        new File(resourcesDir, "META-INF/accesstransformer.cfg") << mergeATs(toMerge)
+        new File("$generatedDir/META-INF/mods.toml") << mergeManifests(toMerge)
+        new File("$generatedDir/META-INF/accesstransformer.cfg") << mergeATs(toMerge)
 
         mergeTags(rootProject, toMerge)
+
+        println("[Mod Merger] Merged mod resources.")
+    }
+
+    static Closure getGeneratedClosure() {
+        return { CopySpec c ->
+            c.include('META-INF/mods.toml', 'META-INF/accesstransformer.cfg', 'data/**')
+        }
     }
 
     private static String mergeManifests(List<Project> toMerge) {
+        println("[Mod Merger - TOMLs] Merged mods.toml's.")
         def modsToml = ""
-        toMerge.each { subproject ->
-            def moduleToml = new File(subproject.sourceSets.main.output.resourcesDir, 'META-INF/mods.toml').text
-            if (modsToml.isEmpty()) {
-                modsToml += moduleToml
-            } else {
-                def splitLines = moduleToml.split('\n')
 
-                for (def i = 4; i < splitLines.length; i++) {
-                    modsToml += '\n' + splitLines[i]
+        for (Project subproject : toMerge) {
+            subproject.sourceSets.main.resources.matching { PatternFilterable pf ->
+                pf.include('META-INF/mods.toml')
+            }.each { file ->
+                if (modsToml.isEmpty()) {
+                    modsToml += file.getText()
+                } else {
+                    def splitLines = file.getText().split('\n')
+
+                    for (def i = 4; i < splitLines.length; i++) {
+                        modsToml += '\n' + splitLines[i]
+                    }
                 }
             }
         }
+
         return modsToml
     }
 
     private static String mergeATs(List<Project> toMerge) {
+        print("[Mod Merger - ATs] Merging access transformers.")
         def ats = ""
         toMerge.each { subproject ->
             def moduleATs = new File(subproject.sourceSets.main.output.resourcesDir, 'META-INF/accesstransformer.cfg')
@@ -77,6 +94,7 @@ class CombineJars {
 
     // More from Mekanism, thank you!
     private static void mergeTags(Project rootProject, List<Project> toMerge) {
+        println("[Mod Merger - Tags] Merging tags")
         Closure tagFilter = { PatternFilterable pf -> pf.include('**/data/*/tags/**/*.json') }
         Map<String, List<String>> reverseTags = new HashMap<>()
         for (Project subProject : toMerge) {
@@ -100,10 +118,11 @@ class CombineJars {
                 mergeTag(rootProject, tag, tagPaths)
             }
         }
+        println("[Mod Merger - Tags] Merged tags.")
     }
 
     private static void mergeTag(Project rootProject, String tag, List<String> tagPaths) {
-        println(tag + " appeared " + tagPaths.size() + " times")
+        println("[Mod Merger - Tags] " + tag + " appeared " + tagPaths.size() + " times")
         Object outputTagAsJson = null
         tagPaths.each { path ->
             Object tagAsJson = new JsonSlurper().parse(rootProject.file(path))
@@ -114,7 +133,7 @@ class CombineJars {
             }
         }
         if (outputTagAsJson != null) {
-            File outputFile = new File("$rootProject.sourceSets.main.output.resourcesDir" + tag)
+            File outputFile = new File("$rootProject.buildDir/generated" + tag)
             //Make all parent directories needed
             outputFile.getParentFile().mkdirs()
             outputFile.text = JsonOutput.toJson(outputTagAsJson)
