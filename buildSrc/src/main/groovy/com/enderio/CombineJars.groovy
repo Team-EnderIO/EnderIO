348037694036d01064fd0cd6d57e8bcfbe3df53f
Rover656 <20544390+Rover656@users.noreply.github.com>
2022-06-01 20:42:30 +0100

    Combined jar

---
+++ buildSrc/src/main/groovy/com/enderio/CombineJars.groovy
@@ -0,0 +1,118 @@
+package com.enderio
+
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import org.gradle.api.Project
+import org.gradle.api.tasks.util.PatternFilterable
+
+// Credit: Mekanism for the base concept.
+
+class CombineJars {
+    static List<String> getPathsToExclude(Project project, boolean mainProject) {
+        List<String> toExclude = new ArrayList<>()
+        toExclude.add('META-INF/mods.toml')
+        toExclude.add('META-INF/accesstransformer.cfg')
+        toExclude.add('.cache/cache')
+
+        // Exclude duplicated data
+        int baseLength = "$project.rootProject.buildDir/resources/main/".length()
+        project.fileTree(dir: "$project.rootProject.buildDir/resources/main/data/").each {
+            File file -> toExclude.add(file.getPath().substring(baseLength))
+        }
+
+        if (!mainProject) {
+            toExclude.add('logo.png')
+            toExclude.add('pack.mcmeta')
+        }
+
+        return toExclude
+    }
+
+    /**
+     * Merge mod resources together.
+     */
+    static void mergeResources(Project rootProject, List<Project> toMerge) {
+        // Create resource directories
+        def resourcesDir = rootProject.sourceSets.main.output.resourcesDir
+        rootProject.mkdir("$resourcesDir/META-INF")
+
+        new File(resourcesDir, "META-INF/mods.toml") << mergeManifests(toMerge)
+        new File(resourcesDir, "META-INF/accesstransformer.cfg") << mergeATs(toMerge)
+
+        mergeTags(rootProject, toMerge)
+    }
+
+    private static String mergeManifests(List<Project> toMerge) {
+        def modsToml = ""
+        toMerge.each { subproject ->
+            def moduleToml = new File(subproject.sourceSets.main.output.resourcesDir, 'META-INF/mods.toml').text
+            if (modsToml.isEmpty()) {
+                modsToml += moduleToml
+            } else {
+                def splitLines = moduleToml.split('\n')
+
+                for (def i = 4; i < splitLines.length; i++) {
+                    modsToml += '\n' + splitLines[i]
+                }
+            }
+        }
+        return modsToml
+    }
+
+    private static String mergeATs(List<Project> toMerge) {
+        def ats = ""
+        toMerge.each { subproject ->
+            def moduleATs = new File(subproject.sourceSets.main.output.resourcesDir, 'META-INF/accesstransformer.cfg')
+            if (moduleATs.exists()) {
+                ats += moduleATs.text
+            }
+        }
+        return ats
+    }
+
+    // More from Mekanism, thank you!
+    private static void mergeTags(Project rootProject, List<Project> toMerge) {
+        Closure tagFilter = { PatternFilterable pf -> pf.include('**/data/*/tags/**/*.json') }
+        Map<String, List<String>> reverseTags = new HashMap<>()
+        for (Project subProject : toMerge) {
+            subProject.sourceSets.main.resources.srcDirs.each { srcDir ->
+                int srcDirPathLength = srcDir.getPath().length()
+                rootProject.fileTree(srcDir).matching(tagFilter).each { file ->
+                    //Add the sourceSet to the reverse lookup
+                    String path = file.getPath()
+                    String tag = path.substring(srcDirPathLength)
+                    if (!reverseTags.containsKey(tag)) {
+                        reverseTags.put(tag, new ArrayList<>())
+                    }
+                    reverseTags.get(tag).add(path)
+                }
+            }
+        }
+        //Go through the reverse tag index and if there are multiple sourceSets that contain the same tag
+        // properly merge that tag
+        reverseTags.each { tag, tagPaths ->
+            if (tagPaths.size() > 1) {
+                mergeTag(rootProject, tag, tagPaths)
+            }
+        }
+    }
+
+    private static void mergeTag(Project rootProject, String tag, List<String> tagPaths) {
+        println(tag + " appeared " + tagPaths.size() + " times")
+        Object outputTagAsJson = null
+        tagPaths.each { path ->
+            Object tagAsJson = new JsonSlurper().parse(rootProject.file(path))
+            if (outputTagAsJson == null) {
+                outputTagAsJson = tagAsJson
+            } else {
+                outputTagAsJson.values += tagAsJson.values
+            }
+        }
+        if (outputTagAsJson != null) {
+            File outputFile = new File("$rootProject.sourceSets.main.output.resourcesDir" + tag)
+            //Make all parent directories needed
+            outputFile.getParentFile().mkdirs()
+            outputFile.text = JsonOutput.toJson(outputTagAsJson)
+        }
+    }
+}
---
+++ doc/modpages/modrinth/enderio-conduits.md
@@ -0,0 +1,18 @@
+# Ender IO Conduits
+Ender IO is a full-featured tech mod. It has armor, tools, weapons, machines, conduits, inventory management, mobs, etc.
+
+## Warning
+Ender IO is being rewritten from the ground up. This module has not yet been started, all features are listed for the 1.12.2 version.
+
+## Features
+This is the module that contains the conduits. It includes:
+- Item conduits
+- Fluid conduits
+- Energy conduits
+- Redstone conduits
+- Capacitor Banks
+- Power Monitors
+
+## Other modules
+- [Base](https://modrinth.com/mod/enderio-base)
+- [Machines](https://modrinth.com/mod/enderio-decor)
