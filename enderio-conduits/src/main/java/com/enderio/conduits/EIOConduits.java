ee38a89f4cf901be5536526e2aa171987db404e1
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2022-06-02 16:55:21 +0100

    Overhaul build scripts (#40)
    * WIP. Working on build scripts.
    
    * Small push to make actions trigger.
    
    * Individual module runs and datagen
    
    * Change push action
    
    * Eclipse plugin missed
    
    * Add appendix and use github run number for build
    
    * Finish up runs
    
    * Remove unused line
    
    * We're gonna use jenkins instead
    
    * Combined jar
    
    * GH actions fix?
    
    * Fix the next issue?
    
    * That should do it?
    
    * This should work.
    
    * Some more work
    
    * More work on modrinth and jar types
    
    * More work, nearly there
    
    * I think thats enough for this PR..
    
    * Automatically set mods for monolith run tasks
    
    * We don't actually need this workaround anymore
    
    * Rid us of annoying warnings

---
+++ enderio-conduits/src/main/java/com/enderio/conduits/EIOConduits.java
@@ -0,0 +1,9 @@
+package com.enderio.conduits;
+
+import net.minecraftforge.fml.common.Mod;
+import org.jetbrains.annotations.NotNull;
+
+@Mod(EIOConduits.MODID)
+public class EIOConduits {
+    public static final @NotNull String MODID = "enderio_conduits";
+}
