38736eb7b8100e8cabd37776cea04c1fc07f9629
agnor99 <kreader100@gmail.com>
2022-07-05 19:07:01 +0200

    TooltipHandler: renamed parameters, removed static context and simplified IAdvancedTooltipProvider finding

--- src/main/java/com/enderio/base/client/tooltip/TooltipHandler.java
+++ src/main/java/com/enderio/base/client/tooltip/TooltipHandler.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
@@ -16,35 +17,36 @@ import net.minecraftforge.fml.common.Mod;
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 // TODO: 1.19: Move to core.
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class TooltipHandler {
 
-    private static final List<Component> tempTooltip = new ArrayList<>();
-
     @SubscribeEvent
     public static void addTooltips(ItemTooltipEvent evt) {
         ItemStack forItem = evt.getItemStack();
-        IAdvancedTooltipProvider provider = null;
-        if (forItem.getItem() instanceof IAdvancedTooltipProvider p) {
-            provider = p;
-        } else if(forItem.getItem() instanceof BlockItem bi && bi.getBlock() instanceof IAdvancedTooltipProvider p) {
-            provider = p;
-        }
-        if (provider != null) {
-            addTooltips(provider, forItem, evt.getPlayer(), evt.getToolTip(), shouldShowAdvancedTooltips());
-        }
+        providerOf(forItem.getItem()).ifPresent(provider ->
+            addTooltips(provider, forItem, evt.getPlayer(), evt.getToolTip(), shouldShowAdvancedTooltips())
+        );
     }
 
-    public static void addTooltips(IAdvancedTooltipProvider tt, ItemStack itemstack, @Nullable Player player, List<Component> list, boolean showAdvanced) {
-        tt.addCommonTooltips(itemstack, player, list);
+    private static Optional<IAdvancedTooltipProvider> providerOf(Item item) {
+        if (item instanceof IAdvancedTooltipProvider provider)
+            return Optional.of(provider);
+        if (item instanceof BlockItem blockItem && blockItem.getBlock() instanceof IAdvancedTooltipProvider provider)
+            return Optional.ofNullable(provider);
+        return Optional.empty();
+    }
+
+    private static void addTooltips(IAdvancedTooltipProvider tooltipProvider, ItemStack itemstack, @Nullable Player player, List<Component> components, boolean showAdvanced) {
+        tooltipProvider.addCommonTooltips(itemstack, player, components);
         if (showAdvanced) {
-            tt.addDetailedTooltips(itemstack, player, list);
+            tooltipProvider.addDetailedTooltips(itemstack, player, components);
         } else {
-            tt.addBasicTooltips(itemstack, player, list);
-            if (hasDetailedTooltip(tt, itemstack, player)) {
-                addShowDetailsTooltip(list);
+            tooltipProvider.addBasicTooltips(itemstack, player, components);
+            if (hasDetailedTooltip(tooltipProvider, itemstack, player)) {
+                addShowDetailsTooltip(components);
             }
         }
     }
@@ -53,13 +55,13 @@ public class TooltipHandler {
         return Screen.hasShiftDown();
     }
 
-    private static void addShowDetailsTooltip(List<Component> list) {
-        list.add(EIOLang.SHOW_DETAIL_TOOLTIP.copy().withStyle(ChatFormatting.WHITE, ChatFormatting.ITALIC));
+    private static void addShowDetailsTooltip(List<Component> components) {
+        components.add(EIOLang.SHOW_DETAIL_TOOLTIP.copy().withStyle(ChatFormatting.WHITE, ChatFormatting.ITALIC));
     }
 
-    private static boolean hasDetailedTooltip(IAdvancedTooltipProvider tt, ItemStack stack, @Nullable Player player) {
-        tempTooltip.clear();
-        tt.addDetailedTooltips(stack, player, tempTooltip);
-        return !tempTooltip.isEmpty();
+    private static boolean hasDetailedTooltip(IAdvancedTooltipProvider tooltipProvider, ItemStack stack, @Nullable Player player) {
+        List<Component> tooltips = new ArrayList<>();
+        tooltipProvider.addDetailedTooltips(stack, player, tooltips);
+        return !tooltips.isEmpty();
     }
 }
