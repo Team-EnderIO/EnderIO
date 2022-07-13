f2e3865908c465965de98a90791a958e7d9d2724
agnor99 <36055315+agnor99@users.noreply.github.com>
2022-07-13 02:51:21 +0200

    SoulVialDispenseBehavior (+ fix for survival disappearing soul vials) (#64)
    * SoulVialDispenseBehavior (+ fix for survival disappearing soul vials)
    
    * Removed Annotation as it's registered in Mod Constructor
    
    * Fix wrong dispenseplacement, drop lead on pickup by dispenser (and for the entities needing the RightClickEvent) and moved new ItemStack calls to Item#getDefaultInstance
    
    * Removed unused import + removed Nonnull + switched Nullable to jetbrains
    
    * Fixed wither+ElderGuardian being capturable by default + iron golem not being in the creative tab
    
    * Update EntityCaptureUtils.java

--- src/main/java/com/enderio/base/common/util/EntityCaptureUtils.java
+++ src/main/java/com/enderio/base/common/util/EntityCaptureUtils.java
@@ -1,12 +1,15 @@
 package com.enderio.base.common.util;
 
+import com.enderio.base.common.lang.EIOLang;
 import com.enderio.base.config.base.BaseConfig;
 import com.enderio.core.common.util.EntityUtil;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +21,7 @@ public class EntityCaptureUtils {
     public static List<ResourceLocation> getCapturableEntities() {
         List<ResourceLocation> entities = new ArrayList<>();
         for (EntityType<?> type : ForgeRegistries.ENTITIES.getValues()) {
-            if (type.getCategory() != MobCategory.MISC) {
+            if (getCapturableStatus(type, null) == CapturableStatus.CAPTURABLE) {
                 ResourceLocation key = ForgeRegistries.ENTITIES.getKey(type);
                 if (key != null && !key.equals(DRAGON)) {
                     entities.add(key);
@@ -29,16 +32,30 @@ public class EntityCaptureUtils {
     }
 
     public enum CapturableStatus {
-        CAPTURABLE,
-        BOSS,
-        BLACKLISTED,
-        INCOMPATIBLE,
+        CAPTURABLE(Component.empty()),
+        BOSS(EIOLang.SOUL_VIAL_ERROR_BOSS),
+        BLACKLISTED(EIOLang.SOUL_VIAL_ERROR_BLACKLISTED),
+        INCOMPATIBLE(EIOLang.SOUL_VIAL_ERROR_FAILED);
+
+        CapturableStatus(Component errorMessage) {
+            this.errorMessage = errorMessage;
+        }
+
+        private final Component errorMessage;
+
+        public Component errorMessage() {
+            return errorMessage;
+        }
     }
 
-    public static CapturableStatus getCapturableStatus(Entity entity) {
-        EntityType<?> type = entity.getType();
+    /**
+     * @param type EntityType to be checked
+     * @param entity The specific entity to be used or null if general information is wanted
+     * @return the status on how this entity should be handled for capture
+     */
+    public static CapturableStatus getCapturableStatus(EntityType<?> type, @Nullable Entity entity) {
 
-        if (isBlacklistedBoss(entity))
+        if (entity != null && isBlacklistedBoss(entity))
             return CapturableStatus.BOSS;
 
         if (!type.canSerialize())
@@ -51,6 +68,10 @@ public class EntityCaptureUtils {
     }
 
     public static boolean isBlacklistedBoss(Entity entity) {
-        return EntityUtil.getEntityType(entity).map(entityType -> EntityUtil.isBoss(entity) && !"minecraft".equals(entityType.getNamespace())).orElse(false);
+        return EntityUtil.getEntityTypeRL(entity)
+            .map(resourceLocation ->
+                EntityUtil.isBoss(entity)
+                && !"minecraft".equals(resourceLocation.getNamespace()))
+            .orElse(false);
     }
 }
