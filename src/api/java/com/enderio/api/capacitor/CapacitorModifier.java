0ad0a04e5410d0412e3a1d1b6a52a1ea7537a3d6
Rover656 <20544390+Rover656@users.noreply.github.com>
2022-08-16 01:06:01 +0100

    Tidy modifier ID generation

--- src/api/java/com/enderio/api/capacitor/CapacitorModifier.java
+++ src/api/java/com/enderio/api/capacitor/CapacitorModifier.java
@@ -6,18 +6,16 @@ import net.minecraft.resources.ResourceLocation;
  * Capacitor key types, for use in loot capacitors for targeting increases to general stats.
  */
 public enum CapacitorModifier {
-    ENERGY_CAPACITY("capacitor.energy_capacity"),
-    ENERGY_USE("capacitor.energy_use"),
-    ENERGY_TRANSFER("capacitor.energy_transfer"),
+    ENERGY_CAPACITY, ENERGY_USE, ENERGY_TRANSFER,
 
     /**
      * @apiNote Capacitors should never multiply the FIXED modifiers...
      */
-    FIXED("capacitor.fixed");
+    FIXED;
 
     public final ResourceLocation id;
 
-    CapacitorModifier(String id) {
-        this.id = new ResourceLocation("enderio", id);
+    CapacitorModifier() {
+        this.id = new ResourceLocation("enderio", "capacitor." + name().toLowerCase());
     }
 }
