bf0bfaea3c7b4445cc4a4ce51042f3ec59938588
Rover656 <20544390+Rover656@users.noreply.github.com>
2022-08-16 01:30:31 +0100

    Add old javadoc info

--- src/api/java/com/enderio/api/capacitor/ICapacitorScalable.java
+++ src/api/java/com/enderio/api/capacitor/ICapacitorScalable.java
@@ -6,6 +6,8 @@ import java.util.function.Supplier;
 
 /**
  * A scalable value is a value which can be scaled based on a capacitor.
+ * This is designed so that things scaled by capacitors can be provided into non-specific domains.
+ * It also allows for the base value of a scalable to be powered by configs.
  */
 public interface ICapacitorScalable {
     Supplier<Float> scaleF(NonNullSupplier<ICapacitorData> data);
