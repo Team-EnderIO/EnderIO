3dabaea89304ec198e770e86b6952af8cd7fef6e
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2022-09-04 20:18:48 +0100

    Merge branch 'dev/1.19.x' into item-decor-composite-models

--- src/decor/java/com/enderio/decoration/client/render/PaintedBlockColor.java
+++ src/decor/java/com/enderio/decoration/client/render/PaintedBlockColor.java
@@ -1,5 +1,6 @@
 package com.enderio.decoration.client.render;
 
+import com.enderio.decoration.common.blockentity.DoublePaintedBlockEntity;
 import com.enderio.decoration.common.blockentity.IPaintableBlockEntity;
 import com.enderio.decoration.common.util.PaintUtils;
 import net.minecraft.client.Minecraft;
@@ -20,15 +21,37 @@ public class PaintedBlockColor implements BlockColor, ItemColor {
     // TODO: Buggy on the sides of blocks. (Grass)
     @Override
     public int getColor(BlockState state, @Nullable BlockAndTintGetter level, @Nullable BlockPos pos, int tintIndex) {
-        if (level != null && pos != null && tintIndex != 0) {
+        if (level != null && pos != null) {
             BlockEntity entity = level.getBlockEntity(pos);
             if (entity instanceof IPaintableBlockEntity paintedBlockEntity) {
                 Block[] paints = paintedBlockEntity.getPaints();
-                for (Block paint : paints) {
+                if (paintedBlockEntity instanceof DoublePaintedBlockEntity doublePaintedBlockEntity) {
+                    if (tintIndex < 0) {
+                        tintIndex = unmoveTintIndex(tintIndex);
+                        if (paints[0] != null) {
+                            BlockState paintState = paints[0].defaultBlockState();
+                            int color = Minecraft.getInstance().getBlockColors().getColor(paintState, level, pos, tintIndex);
+                            if (color != -1)
+                                return color;
+                        }
+                        return 0;
+                    } else {
+                        tintIndex = unmoveTintIndex(tintIndex);
+                        if (paints[1] != null) {
+                            BlockState paintState = paints[1].defaultBlockState();
+                            int color = Minecraft.getInstance().getBlockColors().getColor(paintState, level, pos, tintIndex);
+                            if (color != -1)
+                                return color;
+                        }
+                        return 0;
+                    }
+                }
+                for (int i = 0; i < paints.length; i++) {
+                    Block paint = paints[i];
                     if (paint == null)
                         continue;
                     BlockState paintState = paint.defaultBlockState();
-                    int color = Minecraft.getInstance().getBlockColors().getColor(paintState, level, pos, tintIndex);
+                    int color = Minecraft.getInstance().getBlockColors().getColor(paintState, level, pos, i == 1 ? unmoveTintIndex(tintIndex) : tintIndex);
                     if (color != -1)
                         return color;
                 }
@@ -49,5 +72,17 @@ public class PaintedBlockColor implements BlockColor, ItemColor {
             }
         }
         return 0;
+    }
+
+    //Lets assume that no one uses negative tint indices, so that we can move bottomslab tint indices into the negative half
+    public static int moveTintIndex(int original) {
+        return -original - 2;
+    }
+    public static int unmoveTintIndex(int original) {
+        if (original > 0) {
+            return original;
+        } else {
+            return -original + 2;
+        }
     }
 }
