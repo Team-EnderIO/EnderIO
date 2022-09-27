dd60a6e2a1bcb4b7d5d80c86c2c73dbb10ebb03e
luiq54 <dev.darshanphaldesai@gmail.com>
2022-09-27 18:00:59 +0530

    Box achieved!

--- src/main/java/com/enderio/base/client/particle/RangeParticle.java
+++ src/main/java/com/enderio/base/client/particle/RangeParticle.java
@@ -1,6 +1,5 @@
 package com.enderio.base.client.particle;
 
-import com.enderio.EnderIO;
 import com.enderio.base.common.particle.RangeParticleData;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Vector3f;
@@ -8,7 +7,6 @@ import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 import net.minecraft.core.Direction;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
@@ -17,15 +15,11 @@ import org.jetbrains.annotations.Nullable;
 
 public class RangeParticle extends TextureSheetParticle {
 
-    private static final int INIT_TIME = 20;
-    private static final ResourceLocation WHITE = EnderIO.loc("textures/block/white.png");
-
     private final int range;
 
     public RangeParticle(ClientLevel level, Vec3 pos, int range) {
         super(level, pos.x, pos.y, pos.z);
         this.range = range;
-        //        this.lifetime = 20 * 60 * 10; // 10 minutes
         this.lifetime = 5;
         rCol = 1;
         gCol = 0;
@@ -44,10 +38,15 @@ public class RangeParticle extends TextureSheetParticle {
         var mappedY = (float) (Mth.lerp(partialTicks, this.yo, this.y) - position.y());
         var mappedZ = (float) (Mth.lerp(partialTicks, this.zo, this.z) - position.z());
 
-        var vecTop = new Vector3f(-range, -range, -range);
-        var coords = getLerped(calculateFace(Direction.UP, vecTop.x(), vecTop.y(), vecTop.z(), 2 * range + 1, 2 * range + 1), mappedX, mappedY, mappedZ);
-        renderFace(consumer, coords);
-
+        // Top face requires different z for some reason
+        var vec = new Vector3f(-range, -range, range + 1);
+        renderFace(consumer, remapPosition(calcPoints(Direction.UP, vec), mappedX, mappedY, mappedZ));
+        vec = new Vector3f(-range, -range, -range);
+        renderFace(consumer, remapPosition(calcPoints(Direction.SOUTH, vec), mappedX, mappedY, mappedZ));
+        renderFace(consumer, remapPosition(calcPoints(Direction.EAST, vec), mappedX, mappedY, mappedZ));
+        renderFace(consumer, remapPosition(calcPoints(Direction.UP, vec), mappedX, mappedY, mappedZ));
+        renderFace(consumer, remapPosition(calcPoints(Direction.NORTH, vec), mappedX, mappedY, mappedZ));
+        renderFace(consumer, remapPosition(calcPoints(Direction.WEST, vec), mappedX, mappedY, mappedZ));
     }
 
     @Override
@@ -56,25 +55,29 @@ public class RangeParticle extends TextureSheetParticle {
         return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
     }
 
-    public Vector3f[] calculateFace(Direction face, float x, float y, float z, float w, float h) {
+    public Vector3f[] calcPoints(Direction face, Vector3f vec) {
+        return calcPoints(face, vec.x(), vec.y(), vec.z(), 2 * range + 1, 2 * range + 1);
+    }
+
+    public Vector3f[] calcPoints(Direction face, float x, float y, float z, float w, float h) {
         return switch (face) {
-            case DOWN -> calculateCoordinates(x, x + w, 1.0f - z, 1.0f - z, y, y, y + h, y + h);
-            case UP -> calculateCoordinates(x, x + w, z, z, y + h, y + h, y, y);
-            case NORTH -> calculateCoordinates(x, x + w, y + h, y, z, z, z, z);
-            case SOUTH -> calculateCoordinates(x, x + w, y, y + h, 1.0f - z, 1.0f - z, 1.0f - z, 1.0f - z);
-            case WEST -> calculateCoordinates(1.0f - z, 1.0f - z, y + h, y, x, x + w, x + w, x);
-            case EAST -> calculateCoordinates(z, z, y, y + h, x, x + w, x + w, x);
+            case DOWN -> calcCoordinates(x, x + w, 1.0f - z, 1.0f - z, y, y, y + h, y + h);
+            case UP -> calcCoordinates(x, x + w, z, z, y + h, y + h, y, y);
+            case NORTH -> calcCoordinates(x, x + w, y + h, y, z, z, z, z);
+            case SOUTH -> calcCoordinates(x, x + w, y, y + h, 1.0f - z, 1.0f - z, 1.0f - z, 1.0f - z);
+            case WEST -> calcCoordinates(1.0f - z, 1.0f - z, y + h, y, x, x + w, x + w, x);
+            case EAST -> calcCoordinates(z, z, y, y + h, x, x + w, x + w, x);
         };
     }
 
-    private Vector3f[] getLerped(Vector3f[] coords, float mappedX, float mappedY, float mappedZ) {
+    private Vector3f[] remapPosition(Vector3f[] coords, float mappedX, float mappedY, float mappedZ) {
         for (Vector3f vec : coords) {
             vec.add(mappedX, mappedY, mappedZ);
         }
         return coords;
     }
 
-    private Vector3f[] calculateCoordinates(float x0, float x1, float y0, float y1, float z0, float z1, float z2, float z3) {
+    private Vector3f[] calcCoordinates(float x0, float x1, float y0, float y1, float z0, float z1, float z2, float z3) {
         return new Vector3f[] { new Vector3f(x0, y0, z0), new Vector3f(x1, y0, z1), new Vector3f(x1, y1, z2), new Vector3f(x0, y1, z3) };
     }
 
