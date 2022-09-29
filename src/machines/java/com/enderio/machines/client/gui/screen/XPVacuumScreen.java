70795a3abd501947ec129e7fbf1d729e133ad2ba
luiq54 <dev.darshanphaldesai@gmail.com>
2022-09-29 21:14:43 +0530

    change XP vacuum GUI

--- src/machines/java/com/enderio/machines/client/gui/screen/XPVacuumScreen.java
+++ src/machines/java/com/enderio/machines/client/gui/screen/XPVacuumScreen.java
@@ -4,11 +4,12 @@ import com.enderio.EnderIO;
 import com.enderio.base.common.lang.EIOLang;
 import com.enderio.core.client.gui.screen.EIOScreen;
 import com.enderio.core.client.gui.widgets.EnumIconWidget;
+import com.enderio.core.client.gui.widgets.ToggleImageButton;
 import com.enderio.core.common.util.Vector2i;
 import com.enderio.machines.client.gui.widget.FluidStackStaticWidget;
 import com.enderio.machines.common.menu.XPVacuumMenu;
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
@@ -16,6 +17,8 @@ import net.minecraft.world.entity.player.Inventory;
 public class XPVacuumScreen extends EIOScreen<XPVacuumMenu> {
 
     private static final ResourceLocation XP_VACUUM_BG = EnderIO.loc("textures/gui/xp_vacuum.png");
+    private static final ResourceLocation BUTTONS = EnderIO.loc("textures/gui/icons/buttons.png");
+    private static final ResourceLocation WIDGETS = EnderIO.loc("textures/gui/40/widgetsv2.png");
 
     public XPVacuumScreen(XPVacuumMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle, true);
@@ -26,12 +29,15 @@ public class XPVacuumScreen extends EIOScreen<XPVacuumMenu> {
     protected void init() {
         super.init();
         addRenderableOnly(new FluidStackStaticWidget(this, getMenu().getBlockEntity()::getFluidTank, leftPos + 27, topPos + 22, 32, 32));
-        addRenderableWidget(new EnumIconWidget<>(this, leftPos + imageWidth - 8 - 12, topPos + 50, () -> menu.getBlockEntity().getRedstoneControl(),
+        addRenderableWidget(new EnumIconWidget<>(this, leftPos + imageWidth - 8 - 14, topPos + 52, () -> menu.getBlockEntity().getRedstoneControl(),
             control -> menu.getBlockEntity().setRedstoneControl(control), EIOLang.REDSTONE_MODE));
-        addRenderableWidget(new Button(leftPos + imageWidth - 8 - 12 - 20 - 2, topPos + 50 - 5, 20, 9, Component.literal("\u2303"),
-            (b) -> this.getMenu().getBlockEntity().increaseRange()));
-        addRenderableWidget(new Button(leftPos + imageWidth - 8 - 12 - 20 - 2, topPos + 50 + 6, 20, 9, Component.literal("\u2304"),
-            (b) -> this.getMenu().getBlockEntity().decreaseRange()));
+        //TODO tooltips
+        addRenderableWidget(new ToggleImageButton<>(this, leftPos + imageWidth - 8 - 16, topPos + 34, 16, 16, 144, 176, 16, 0, WIDGETS,
+            () -> menu.getBlockEntity().isShowingRange(), state -> menu.getBlockEntity().shouldShowRange(state)));
+        addRenderableWidget(
+            new ImageButton(leftPos + imageWidth - 8 - 8 - 2 - 16, topPos + 34, 8, 8, 8, 0, 16, BUTTONS, (b) -> this.menu.getBlockEntity().increaseRange()));
+        addRenderableWidget(
+            new ImageButton(leftPos + imageWidth - 8 - 8 - 2 - 16, topPos + 42, 8, 8, 8, 8, 16, BUTTONS, (b) -> this.menu.getBlockEntity().decreaseRange()));
     }
 
     @Override
@@ -45,9 +51,15 @@ public class XPVacuumScreen extends EIOScreen<XPVacuumMenu> {
     }
 
     @Override
+    protected void renderLabels(PoseStack pPoseStack, int pMouseX, int pMouseY) {
+        this.font.draw(pPoseStack, EIOLang.RANGE, this.imageWidth - 8 - this.font.width(EIOLang.RANGE), 21, 4210752);
+        super.renderLabels(pPoseStack, pMouseX, pMouseY);
+    }
+
+    @Override
     public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTicks) {
         super.render(pPoseStack, pMouseX, pMouseY, pPartialTicks);
-        font.draw(pPoseStack, this.getMenu().getBlockEntity().getRange() + "", leftPos + imageWidth - 8 - 12 - 20 - 2 - 8, topPos + 50, 0);
+        font.draw(pPoseStack, this.getMenu().getBlockEntity().getRange() + "", leftPos + imageWidth - 8 - 16 - 2 - 8 - 10, topPos + 38, 0);
     }
 
 }
