d345b635bb3dd25d2c4bc2adbdca0eaa4cb8084e
luiq54 <dev.darshanphaldesai@gmail.com>
2022-12-02 15:58:37 +0530

    change deprecated capability

--- src/machines/java/com/enderio/machines/common/blockentity/XPVacuumBlockEntity.java
+++ src/machines/java/com/enderio/machines/common/blockentity/XPVacuumBlockEntity.java
@@ -17,9 +17,9 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fluids.FluidStack;
-import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandler.FluidAction;
 import net.minecraftforge.fluids.capability.templates.FluidTank;
 import org.jetbrains.annotations.NotNull;
@@ -88,7 +88,7 @@ public class XPVacuumBlockEntity extends VacuumMachineEntity<ExperienceOrb> {
     @NotNull
     @Override
     public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
-        if (cap == CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY && side == null) {
+        if (cap == ForgeCapabilities.FLUID_HANDLER && side == null) {
             return fluidHandlerCap.cast();
         }
 
