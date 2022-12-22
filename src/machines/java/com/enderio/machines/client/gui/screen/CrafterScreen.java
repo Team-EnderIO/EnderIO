01b13e76571e87e1eadcc04ff0bb69a0a04e07f4
luiq54 <dev.darshanphaldesai@gmail.com>
2022-12-22 16:44:10 +0530

    redstone control

--- src/machines/java/com/enderio/machines/client/gui/screen/CrafterScreen.java
+++ src/machines/java/com/enderio/machines/client/gui/screen/CrafterScreen.java
@@ -1,7 +1,9 @@
 package com.enderio.machines.client.gui.screen;
 
 import com.enderio.EnderIO;
+import com.enderio.base.common.lang.EIOLang;
 import com.enderio.core.client.gui.screen.EIOScreen;
+import com.enderio.core.client.gui.widgets.EnumIconWidget;
 import com.enderio.core.common.util.Vector2i;
 import com.enderio.machines.client.gui.widget.EnergyWidget;
 import com.enderio.machines.common.menu.CrafterMenu;
@@ -23,8 +25,8 @@ public class CrafterScreen extends EIOScreen<CrafterMenu> {
         super.init();
         addRenderableOnly(new EnergyWidget(this, getMenu().getBlockEntity()::getEnergyStorage, 10 + leftPos, 14 + topPos, 9, 42));
 
-        //        addRenderableWidget(new EnumIconWidget<>(this, leftPos + imageWidth - 8 - 12, topPos + 6, () -> menu.getBlockEntity().getRedstoneControl(),
-        //            control -> menu.getBlockEntity().setRedstoneControl(control), EIOLang.REDSTONE_MODE));
+        addRenderableWidget(new EnumIconWidget<>(this, leftPos + imageWidth - 5 - 16, topPos + 5, () -> menu.getBlockEntity().getRedstoneControl(),
+            control -> menu.getBlockEntity().setRedstoneControl(control), EIOLang.REDSTONE_MODE));
     }
 
     @Override
