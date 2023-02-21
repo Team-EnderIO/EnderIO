6bf89655eb2d3f52dbc1bcddf364abc45b59034e
agnor99 <kreader100@gmail.com>
2023-02-21 01:19:03 +0100

    add sound

--- src/main/java/com/enderio/base/common/hangglider/PlayerMovementHandler.java
+++ src/main/java/com/enderio/base/common/hangglider/PlayerMovementHandler.java
@@ -4,8 +4,18 @@ import com.enderio.api.glider.GliderMovementInfo;
 import com.enderio.api.integration.IntegrationManager;
 import com.enderio.base.common.advancement.UseGliderTrigger;
 import com.enderio.base.common.lang.EIOLang;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.resources.sounds.AbstractTickableSoundInstance;
+import net.minecraft.client.resources.sounds.ElytraOnPlayerSoundInstance;
+import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.TickEvent;
@@ -14,6 +24,7 @@ import net.minecraftforge.fml.common.Mod;
 
 import java.util.Map;
 import java.util.Optional;
+import java.util.Random;
 import java.util.WeakHashMap;
 
 @Mod.EventBusSubscriber
@@ -61,11 +72,11 @@ public class PlayerMovementHandler {
             newDeltaMovement = newDeltaMovement.scale(AIR_FRICTION_COEFFICIENT);
             player.setDeltaMovement(newDeltaMovement);
             player.fallDistance = 0f;
-            if (!player.level.isClientSide()) {
-                player.hurtMarked = true;
-            }
             if (player instanceof ServerPlayer serverPlayer) {
                 UseGliderTrigger.USE_GLIDER.trigger(serverPlayer);
+                player.hurtMarked = true;
+            } else if (player.getLevel().isClientSide()) {
+                ClientClassLoadingProtection.playSound(player);
             }
             gliderMovementInfo.cause().onHangGliderTick(player);
         }
@@ -86,5 +97,57 @@ public class PlayerMovementHandler {
         }
 
         return Optional.empty();
+    }
+
+    private static class ClientClassLoadingProtection {
+        private static void playSound(Player player) {
+            if (player instanceof LocalPlayer localPlayer && TICKS_FALLING.getOrDefault(player, 0) == 13) {
+                Minecraft.getInstance().getSoundManager().play(new WindSoundInstance(localPlayer));
+            }
+        }
+    }
+
+    private static class WindSoundInstance extends AbstractTickableSoundInstance {
+        private final LocalPlayer player;
+        private int time;
+        public WindSoundInstance(LocalPlayer player) {
+            super(SoundEvents.ELYTRA_FLYING, SoundSource.PLAYERS, SoundInstance.createUnseededRandom());
+            this.player = player;
+            this.looping = true;
+            this.delay = 0;
+            this.volume = 0.1F;
+        }
+
+        @Override
+        public void tick() {
+
+            ++this.time;
+            if (!this.player.isRemoved() && (this.time <= 20 || PlayerMovementHandler.calculateGliderMovementInfo(player, false).isPresent())) {
+                this.x = this.player.getX();
+                this.y = this.player.getY();
+                this.z = this.player.getZ();
+                float f = (float)this.player.getDeltaMovement().lengthSqr();
+                if (f >= 1.0E-7D) {
+                    this.volume = Mth.clamp(f / 4.0F, 0.0F, 1.0F);
+                } else {
+                    this.volume = 0.0F;
+                }
+
+                if (this.time < 20) {
+                    this.volume = 0.0F;
+                } else if (this.time < 40) {
+                    this.volume *= (this.time - 20) / 20.0F;
+                }
+
+                if (this.volume > 0.8F) {
+                    this.pitch = 1.0F + (this.volume - 0.8F);
+                } else {
+                    this.pitch = 1.0F;
+                }
+                volume *= 0.5;
+            } else {
+                this.stop();
+            }
+        }
     }
 }
