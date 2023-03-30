17c823f0ddf663ea3f0d53ae5a330983048ddbd0
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2023-03-30 14:55:45 +0100

    Finish merge

--- src/main/java/com/enderio/base/data/recipe/MultipleCookingRecipeBuilder.java
+++ src/main/java/com/enderio/base/data/recipe/MultipleCookingRecipeBuilder.java
@@ -8,20 +8,22 @@ import net.minecraft.advancements.RequirementsStrategy;
 import net.minecraft.advancements.critereon.RecipeUnlockedTrigger;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeBuilder;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.SimpleCookingRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.AbstractCookingRecipe;
-import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.SimpleCookingSerializer;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nullable;
 import java.util.function.Consumer;
 
 public class MultipleCookingRecipeBuilder implements RecipeBuilder {
-
+    private final RecipeCategory category;
+    private final CookingBookCategory bookCategory;
     private final ItemStack result;
     private final Ingredient ingredient;
     private final float experience;
@@ -29,10 +31,12 @@ public class MultipleCookingRecipeBuilder implements RecipeBuilder {
     private final Advancement.Builder advancement = Advancement.Builder.advancement();
     @Nullable
     private String group;
-    private final SimpleCookingSerializer<?> serializer;
+    private final RecipeSerializer<? extends AbstractCookingRecipe> serializer;
 
-    private MultipleCookingRecipeBuilder(ItemStack result, Ingredient ingredient, float experience, int cookingTime,
-        SimpleCookingSerializer<?> serializer) {
+    private MultipleCookingRecipeBuilder(RecipeCategory pCategory, CookingBookCategory pBookCategory, ItemStack result, Ingredient ingredient, float experience, int cookingTime,
+        RecipeSerializer<? extends AbstractCookingRecipe> serializer) {
+        this.category = pCategory;
+        this.bookCategory = pBookCategory;
         this.result = result;
         this.ingredient = ingredient;
         this.experience = experience;
@@ -40,24 +44,24 @@ public class MultipleCookingRecipeBuilder implements RecipeBuilder {
         this.serializer = serializer;
     }
 
-    public static MultipleCookingRecipeBuilder cooking(Ingredient pIngredient, ItemStack pResult, float pExperience, int pCookingTime, SimpleCookingSerializer<?> pSerializer) {
-        return new MultipleCookingRecipeBuilder(pResult, pIngredient, pExperience, pCookingTime, pSerializer);
+    public static MultipleCookingRecipeBuilder generic(Ingredient pIngredient, RecipeCategory pCategory, ItemStack pResult, float pExperience, int pCookingTime, RecipeSerializer<? extends AbstractCookingRecipe> pSerializer) {
+        return new MultipleCookingRecipeBuilder(pCategory, determineRecipeCategory(pSerializer, pResult), pResult, pIngredient, pExperience, pCookingTime, pSerializer);
     }
 
-    public static MultipleCookingRecipeBuilder campfireCooking(Ingredient pIngredient, ItemStack pResult, float pExperience, int pCookingTime) {
-        return cooking(pIngredient, pResult, pExperience, pCookingTime, RecipeSerializer.CAMPFIRE_COOKING_RECIPE);
+    public static MultipleCookingRecipeBuilder campfireCooking(Ingredient pIngredient, RecipeCategory pCategory, ItemStack pResult, float pExperience, int pCookingTime) {
+        return new MultipleCookingRecipeBuilder(pCategory, CookingBookCategory.FOOD, pResult, pIngredient, pExperience, pCookingTime, RecipeSerializer.CAMPFIRE_COOKING_RECIPE);
     }
 
-    public static MultipleCookingRecipeBuilder blasting(Ingredient pIngredient, ItemStack pResult, float pExperience, int pCookingTime) {
-        return cooking(pIngredient, pResult, pExperience, pCookingTime, RecipeSerializer.BLASTING_RECIPE);
+    public static MultipleCookingRecipeBuilder blasting(Ingredient pIngredient, RecipeCategory pCategory, ItemStack pResult, float pExperience, int pCookingTime) {
+        return new MultipleCookingRecipeBuilder(pCategory, determineBlastingRecipeCategory(pResult), pResult, pIngredient, pExperience, pCookingTime, RecipeSerializer.BLASTING_RECIPE);
     }
 
-    public static MultipleCookingRecipeBuilder smelting(Ingredient pIngredient, ItemStack pResult, float pExperience, int pCookingTime) {
-        return cooking(pIngredient, pResult, pExperience, pCookingTime, RecipeSerializer.SMELTING_RECIPE);
+    public static MultipleCookingRecipeBuilder smelting(Ingredient pIngredient, RecipeCategory pCategory, ItemStack pResult, float pExperience, int pCookingTime) {
+        return new MultipleCookingRecipeBuilder(pCategory, determineSmeltingRecipeCategory(pResult), pResult, pIngredient, pExperience, pCookingTime, RecipeSerializer.SMELTING_RECIPE);
     }
 
-    public static MultipleCookingRecipeBuilder smoking(Ingredient pIngredient, ItemStack pResult, float pExperience, int pCookingTime) {
-        return cooking(pIngredient, pResult, pExperience, pCookingTime, RecipeSerializer.SMOKING_RECIPE);
+    public static MultipleCookingRecipeBuilder smoking(Ingredient pIngredient, RecipeCategory pCategory, ItemStack pResult, float pExperience, int pCookingTime) {
+        return new MultipleCookingRecipeBuilder(pCategory, CookingBookCategory.FOOD, pResult, pIngredient, pExperience, pCookingTime, RecipeSerializer.SMOKING_RECIPE);
     }
 
     public MultipleCookingRecipeBuilder unlockedBy(String pCriterionName, CriterionTriggerInstance pCriterionTrigger) {
@@ -70,17 +74,39 @@ public class MultipleCookingRecipeBuilder implements RecipeBuilder {
         return this;
     }
 
-    @Override
     public Item getResult() {
         return result.getItem();
     }
 
-    @Override
     public void save(Consumer<FinishedRecipe> pFinishedRecipeConsumer, ResourceLocation pRecipeId) {
         this.ensureValid(pRecipeId);
-        this.advancement.parent(ROOT_RECIPE_ADVANCEMENT).addCriterion("has_the_recipe", RecipeUnlockedTrigger.unlocked(pRecipeId)).rewards(AdvancementRewards.Builder.recipe(pRecipeId)).requirements(
-            RequirementsStrategy.OR);
-        pFinishedRecipeConsumer.accept(new Result(pRecipeId, this.group == null ? "" : this.group, this.ingredient, this.result, this.experience, this.cookingTime, this.advancement, new ResourceLocation(pRecipeId.getNamespace(), "recipes/" + this.result.getItem().getItemCategory().getRecipeFolderName() + "/" + pRecipeId.getPath()), this.serializer));
+        this.advancement.parent(ROOT_RECIPE_ADVANCEMENT).addCriterion("has_the_recipe", RecipeUnlockedTrigger.unlocked(pRecipeId)).rewards(AdvancementRewards.Builder.recipe(pRecipeId)).requirements(RequirementsStrategy.OR);
+        pFinishedRecipeConsumer.accept(new Result(pRecipeId, this.group == null ? "" : this.group, this.bookCategory, this.ingredient, this.result, this.experience, this.cookingTime, this.advancement, pRecipeId.withPrefix("recipes/" + this.category.getFolderName() + "/"), this.serializer));
+    }
+
+    // TODO: 1.19.4: ATs for this
+    private static CookingBookCategory determineSmeltingRecipeCategory(ItemStack pResult) {
+        if (pResult.getItem().isEdible()) {
+            return CookingBookCategory.FOOD;
+        } else {
+            return pResult.getItem() instanceof BlockItem ? CookingBookCategory.BLOCKS : CookingBookCategory.MISC;
+        }
+    }
+
+    private static CookingBookCategory determineBlastingRecipeCategory(ItemStack pResult) {
+        return pResult.getItem() instanceof BlockItem ? CookingBookCategory.BLOCKS : CookingBookCategory.MISC;
+    }
+
+    private static CookingBookCategory determineRecipeCategory(RecipeSerializer<? extends AbstractCookingRecipe> pSerializer, ItemStack pResult) {
+        if (pSerializer == RecipeSerializer.SMELTING_RECIPE) {
+            return determineSmeltingRecipeCategory(pResult);
+        } else if (pSerializer == RecipeSerializer.BLASTING_RECIPE) {
+            return determineBlastingRecipeCategory(pResult);
+        } else if (pSerializer != RecipeSerializer.SMOKING_RECIPE && pSerializer != RecipeSerializer.CAMPFIRE_COOKING_RECIPE) {
+            throw new IllegalStateException("Unknown cooking recipe type");
+        } else {
+            return CookingBookCategory.FOOD;
+        }
     }
 
     private void ensureValid(ResourceLocation pId) {
@@ -92,6 +118,7 @@ public class MultipleCookingRecipeBuilder implements RecipeBuilder {
     public static class Result implements FinishedRecipe {
         private final ResourceLocation id;
         private final String group;
+        private final CookingBookCategory category;
         private final Ingredient ingredient;
         private final ItemStack result;
         private final float experience;
@@ -100,9 +127,10 @@ public class MultipleCookingRecipeBuilder implements RecipeBuilder {
         private final ResourceLocation advancementId;
         private final RecipeSerializer<? extends AbstractCookingRecipe> serializer;
 
-        public Result(ResourceLocation pId, String pGroup, Ingredient pIngredient, ItemStack pResult, float pExperience, int pCookingTime, Advancement.Builder pAdvancement, ResourceLocation pAdvancementId, RecipeSerializer<? extends AbstractCookingRecipe> pSerializer) {
+        public Result(ResourceLocation pId, String pGroup, CookingBookCategory pCategory, Ingredient pIngredient, ItemStack pResult, float pExperience, int pCookingTime, Advancement.Builder pAdvancement, ResourceLocation pAdvancementId, RecipeSerializer<? extends AbstractCookingRecipe> pSerializer) {
             this.id = pId;
             this.group = pGroup;
+            this.category = pCategory;
             this.ingredient = pIngredient;
             this.result = pResult;
             this.experience = pExperience;
@@ -117,6 +145,7 @@ public class MultipleCookingRecipeBuilder implements RecipeBuilder {
                 pJson.addProperty("group", this.group);
             }
 
+            pJson.addProperty("category", this.category.getSerializedName());
             pJson.add("ingredient", this.ingredient.toJson());
 
             JsonObject resultObj = new JsonObject();
