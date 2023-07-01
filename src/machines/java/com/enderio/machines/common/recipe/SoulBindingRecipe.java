112b906896c26f1f28c2195391a17a7510eda66b
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2023-07-01 17:31:00 +0100

    Fix running dedicated server

--- src/machines/java/com/enderio/machines/common/recipe/SoulBindingRecipe.java
+++ src/machines/java/com/enderio/machines/common/recipe/SoulBindingRecipe.java
@@ -217,15 +217,19 @@ public class SoulBindingRecipe implements MachineRecipe<SoulBindingRecipe.Contai
                 int exp = buffer.readInt();
 
                 ResourceLocation entityType = null;
-                try { //fails if not present
-                    entityType = buffer.readResourceLocation();
-                } catch (Exception ignored) {
+                if (buffer.readBoolean()) {
+                    try { //fails if not present
+                        entityType = buffer.readResourceLocation();
+                    } catch (Exception ignored) {
+                    }
                 }
 
                 MobCategory mobCategory = null;
-                try { //fails if not present
-                    mobCategory = MobCategory.byName(buffer.readUtf());
-                } catch (Exception ignored) {
+                if (buffer.readBoolean()) {
+                    try { //fails if not present
+                        mobCategory = MobCategory.byName(buffer.readUtf());
+                    } catch (Exception ignored) {
+                    }
                 }
 
                 return new SoulBindingRecipe(recipeId, output, input, energy, exp, entityType, mobCategory);
@@ -242,9 +246,13 @@ public class SoulBindingRecipe implements MachineRecipe<SoulBindingRecipe.Contai
                 recipe.input.toNetwork(buffer);
                 buffer.writeInt(recipe.energy);
                 buffer.writeInt(recipe.exp);
+
+                buffer.writeBoolean(recipe.entityType != null);
                 if (recipe.entityType != null) { //don't write null
                     buffer.writeResourceLocation(recipe.entityType);
                 }
+
+                buffer.writeBoolean(recipe.mobCategory != null);
                 if (recipe.mobCategory != null) { //don't write null
                     buffer.writeUtf(recipe.mobCategory.getName());
                 }
