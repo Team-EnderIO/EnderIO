1aec870ab7ab7802f34f4d791e09477d0721f02e
agnor99 <36055315+agnor99@users.noreply.github.com>
2023-07-03 20:19:48 +0200

    rename network channel

--- src/main/java/com/enderio/base/common/network/EIONetwork.java
+++ src/main/java/com/enderio/base/common/network/EIONetwork.java
@@ -1,5 +1,6 @@
 package com.enderio.base.common.network;
 
+import com.enderio.EnderIO;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
@@ -7,8 +8,7 @@ import net.minecraftforge.network.simple.SimpleChannel;
 public class EIONetwork {
     private static final String PROTOCOL_VERSION = "1";
 
-    public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
-            new ResourceLocation("enderdecore", "main"),
+    public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(EnderIO.loc("main"),
             () -> PROTOCOL_VERSION,
             PROTOCOL_VERSION::equals,
             PROTOCOL_VERSION::equals
