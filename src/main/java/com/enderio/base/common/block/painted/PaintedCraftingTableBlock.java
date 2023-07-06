e8b6e7ae5729e71cbd9e060e14668a6ec0a9314b
agnor99 <36055315+agnor99@users.noreply.github.com>
2023-07-06 17:58:00 +0200

    appearance

--- src/main/java/com/enderio/base/common/block/painted/PaintedCraftingTableBlock.java
+++ src/main/java/com/enderio/base/common/block/painted/PaintedCraftingTableBlock.java
@@ -1,8 +1,10 @@
 package com.enderio.base.common.block.painted;
 
+import com.enderio.base.common.blockentity.SinglePaintedBlockEntity;
 import com.enderio.base.common.init.EIOBlockEntities;
 import com.enderio.base.common.init.EIOBlocks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.SimpleMenuProvider;
@@ -10,6 +12,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.CraftingMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.CraftingTableBlock;
@@ -50,5 +53,14 @@ public class PaintedCraftingTableBlock extends CraftingTableBlock implements Ent
     @Override
     public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter level, BlockPos pos, Player player) {
         return getPaintedStack(level, pos, this);
+    }
+
+    @Override
+    public BlockState getAppearance(BlockState state, BlockAndTintGetter level, BlockPos pos, Direction side, @Nullable BlockState queryState,
+        @Nullable BlockPos queryPos) {
+        if (level.getBlockEntity(pos) instanceof SinglePaintedBlockEntity painted && painted.getPaint() != null) {
+            return painted.getPaint().defaultBlockState();
+        }
+        return super.getAppearance(state, level, pos, side, queryState, queryPos);
     }
 }
