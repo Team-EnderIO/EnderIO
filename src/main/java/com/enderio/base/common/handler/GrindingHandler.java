05c82eb5039d4b4c8110b80db446e61ac8183c7c
NathanDSimon <simon.nathand@gmail.com>
2023-07-10 13:06:55 -0400

    Fix more formatting; change to use BlockState::is calls instead of the == operator

--- src/main/java/com/enderio/base/common/handler/GrindingHandler.java
+++ src/main/java/com/enderio/base/common/handler/GrindingHandler.java
@@ -6,8 +6,8 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -17,23 +17,23 @@ public class GrindingHandler {
     @SubscribeEvent
     public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
         Player player = event.getEntity();
-        Block target = event.getLevel().getBlockState(event.getPos()).getBlock();
+        if (!player.isCrouching())
+            return;
+        BlockState target = event.getLevel().getBlockState(event.getPos());
         ItemStack mainhand = player.getMainHandItem();
         ItemStack offhand = player.getOffhandItem();
-        if (!(player.isCrouching())
-            || (target != Blocks.OBSIDIAN && target != Blocks.GRINDSTONE && target != Blocks.CRYING_OBSIDIAN)
-            || !(offhand.is(Items.FLINT))) {
+        if(!offhand.is(Items.FLINT) &&
+            (mainhand.is(Items.DEEPSLATE) || mainhand.is(Items.COBBLED_DEEPSLATE) || mainhand.is(Items.COAL)))
             return;
-        }
         if (mainhand.is(Items.DEEPSLATE) || mainhand.is(Items.COBBLED_DEEPSLATE)) {
             mainhand.shrink(1);
             // Grindstone consumes Flint 33% of the time
-            if (target != Blocks.OBSIDIAN && target != Blocks.CRYING_OBSIDIAN && RandomSource.create().nextInt(3) == 0) {
+            if (target.is(Blocks.GRINDSTONE) && RandomSource.create().nextInt(3) == 0) {
                 offhand.shrink(1);
             }
             int output = 1;
             // Crying Obsidian has a 33% chance to double the output
-            if (target == Blocks.CRYING_OBSIDIAN && RandomSource.create().nextInt(3) == 0) {
+            if (target.is(Blocks.CRYING_OBSIDIAN) && RandomSource.create().nextInt(3) == 0) {
                 output = 2;
             }
             player.addItem(new ItemStack(EIOItems.GRAINS_OF_INFINITY.get(), output));
@@ -41,19 +41,16 @@ public class GrindingHandler {
         } else if (mainhand.is(Items.COAL) && mainhand.getCount() >= 3) {
             int cost = 3;
             // Crying Obsidian had a 33% chance to only consume 1 coal instead of 3
-            if (target == Blocks.CRYING_OBSIDIAN && RandomSource.create().nextInt(3) == 0) {
+            if (target.is(Blocks.CRYING_OBSIDIAN) && RandomSource.create().nextInt(3) == 0) {
                 cost = 1;
             }
             mainhand.shrink(cost);
             // Grindstone consumes Flint 33% of the time
-            if (target != Blocks.OBSIDIAN && target != Blocks.CRYING_OBSIDIAN && RandomSource.create().nextInt(3) == 0) {
+            if (target.is(Blocks.GRINDSTONE) && RandomSource.create().nextInt(3) == 0) {
                 offhand.shrink(1);
             }
             player.addItem(new ItemStack(EIOItems.POWDERED_COAL.get(), 1));
         }
-        // Cancel the event so that we don't bring up the GUI for the grindstone. Unnecessary for obsidian
-        if (target == Blocks.GRINDSTONE) {
-            event.setCanceled(true);
-        }
+        event.setCanceled(true);
     }
 }
