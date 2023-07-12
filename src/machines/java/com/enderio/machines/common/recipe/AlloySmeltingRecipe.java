c08b9fc74484ab67ede48f33d7b616a4efc1e0cc
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2023-07-12 17:54:12 +0100

    Sort #294

--- src/machines/java/com/enderio/machines/common/recipe/AlloySmeltingRecipe.java
+++ src/machines/java/com/enderio/machines/common/recipe/AlloySmeltingRecipe.java
@@ -148,7 +148,7 @@ public class AlloySmeltingRecipe implements MachineRecipe<AlloySmeltingRecipe.Co
             }
 
             // Load result, energy and experience.
-            ItemStack result = CraftingHelper.getItemStack(serializedRecipe.getAsJsonObject("result"), false);
+            ItemStack result = CraftingHelper.getItemStack(serializedRecipe.getAsJsonObject("result"), true);
             int energy = serializedRecipe.get("energy").getAsInt();
             float experience = serializedRecipe.get("experience").getAsFloat();
             return new AlloySmeltingRecipe(recipeId, inputs, result, energy, experience);
