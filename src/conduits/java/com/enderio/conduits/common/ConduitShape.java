9acfd868ae5df6ecbf5733655f793bf2275fbcc2
agnor99 <36055315+agnor99@users.noreply.github.com>
2023-07-14 22:46:19 +0200

    fixes 254

--- src/conduits/java/com/enderio/conduits/common/ConduitShape.java
+++ src/conduits/java/com/enderio/conduits/common/ConduitShape.java
@@ -3,6 +3,7 @@ package com.enderio.conduits.common;
 import com.enderio.api.conduit.IConduitType;
 import com.enderio.conduits.common.blockentity.ConduitBundle;
 import com.enderio.conduits.common.blockentity.OffsetHelper;
+import com.enderio.conduits.common.blockentity.connection.DynamicConnectionState;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Vec3i;
@@ -83,7 +84,7 @@ public class ConduitShape {
         Map<IConduitType<?>, List<Vec3i>> offsets = new HashMap<>();
         for (Direction direction : Direction.values()) {
             VoxelShape directionShape = directionShapes.getOrDefault(direction, Shapes.empty());
-            if (conduitBundle.getConnection(direction).isEnd()) {
+            if (conduitBundle.getConnection(direction).getConnectionState(conduitType, conduitBundle) instanceof DynamicConnectionState) {
                 VoxelShape connectorShape = rotateVoxelShape(connector, direction);
                 directionShape = Shapes.join(directionShape, connectorShape, BooleanOp.OR);
                 conduitShape = Shapes.join(conduitShape, connectorShape, BooleanOp.OR);
