7ba436d60f379ae67fdfc8618182375deb4d01cc
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2023-08-20 01:53:34 +0200

    Update MachinePackets.java

--- src/machines/java/com/enderio/machines/common/init/MachinePackets.java
+++ src/machines/java/com/enderio/machines/common/init/MachinePackets.java
@@ -1,18 +1,21 @@
 package com.enderio.machines.common.init;
 
-import com.enderio.base.common.network.SyncTravelDataPacket;
 import com.enderio.core.common.network.CoreNetwork;
 import com.enderio.machines.common.network.PoweredSpawnerSoulPacket;
+import com.enderio.machines.common.network.SoulEngineSoulPacket;
 import com.enderio.machines.common.network.UpdateCrafterTemplatePacket;
+import com.enderio.machines.common.souldata.EngineSoul;
 import com.enderio.machines.common.souldata.SpawnerSoul;
 
 public class MachinePackets {
     public static void register() {
         //Sync soul data (optional)
         SpawnerSoul.SPAWNER.subscribeAsSyncable(PoweredSpawnerSoulPacket::new);
+        EngineSoul.ENGINE.subscribeAsSyncable(SoulEngineSoulPacket::new);
 
         //packets
         CoreNetwork.registerPacket(new UpdateCrafterTemplatePacket.Handler(), UpdateCrafterTemplatePacket.class);
         CoreNetwork.registerPacket(new PoweredSpawnerSoulPacket.Handler(), PoweredSpawnerSoulPacket.class);
+        CoreNetwork.registerPacket(new SoulEngineSoulPacket.Handler(), SoulEngineSoulPacket.class);
     }
 }
