bf364148470feed3118c7fa48f0a385abff77742
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2023-08-22 00:54:45 +0200

    Some Buffs to the spawner and Primitive Alloy Smelter

--- src/machines/java/com/enderio/machines/common/config/common/MachinesCommonConfig.java
+++ src/machines/java/com/enderio/machines/common/config/common/MachinesCommonConfig.java
@@ -11,6 +11,7 @@ public class MachinesCommonConfig {
     public final ForgeConfigSpec.ConfigValue<Integer> MAX_SPAWNER_ENTITIES;
     public final ForgeConfigSpec.ConfigValue<Integer> MAX_SPAWNERS;
     public final ForgeConfigSpec.ConfigValue<SpawnerMachineTask.SpawnType> SPAWN_TYPE;
+    public final ForgeConfigSpec.IntValue SPAWN_AMOUNT;
 
     public MachinesCommonConfig(ForgeConfigSpec.Builder builder) {
         ENERGY = new EnergyConfig(builder);
@@ -22,9 +23,10 @@ public class MachinesCommonConfig {
         builder.pop();
 
         builder.push("poweredSpawner");
-        MAX_SPAWNER_ENTITIES = builder.comment("The amount of entities that will turn off powered spawner if in range.").define("maxentities", 2);
-        SPAWN_TYPE = builder.comment("The way a powered spawner spawns an entity. Possible options: 'ENTITYPE' Spawns the same entity types as the soul vial. 'COPY' Spawns an exact copy of the mob in the soul vial").define("Spawn Type", SpawnerMachineTask.SpawnType.ENTITYTYPE);
-        MAX_SPAWNERS = builder.comment("The maximum amount of spawners before the spawners suffers a loss of efficiency").define("maxspawners", 10);
+            SPAWN_AMOUNT = builder.comment("The amount of mobs that spawn from the spawner").defineInRange("spawn_amount", 4, 0, Integer.MAX_VALUE);
+            MAX_SPAWNER_ENTITIES = builder.comment("The amount of entities that will turn off powered spawner if in range.").defineInRange("maxentities", 2, 0, Integer.MAX_VALUE);
+            SPAWN_TYPE = builder.comment("The way a powered spawner spawns an entity. Possible options: 'ENTITYPE' Spawns the same entity types as the soul vial. 'COPY' Spawns an exact copy of the mob in the soul vial").define("Spawn Type", SpawnerMachineTask.SpawnType.ENTITYTYPE);
+            MAX_SPAWNERS = builder.comment("The maximum amount of spawners before the spawners suffers a loss of efficiency").defineInRange("maxspawners", 10, 0, Integer.MAX_VALUE);
         builder.pop();
     }
 }
