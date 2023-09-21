f12789e5a48af4891f05de841ab2ab432ef026a5
agnor99 <36055315+agnor99@users.noreply.github.com>
2023-09-21 20:21:23 +0200

    remove import

--- src/api/java/com/enderio/api/integration/Integration.java
+++ src/api/java/com/enderio/api/integration/Integration.java
@@ -5,7 +5,6 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.SmeltingRecipe;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
