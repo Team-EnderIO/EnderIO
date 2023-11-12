0e837c9ea90fd3b177d50863eff680bf4167d1d8
mystchonky <dev.darshanphaldesai@gmail.com>
2023-11-12 19:29:52 +0530

    chore: Rework tank logic.
    Backport of #561
    
    * Initial tank layout
    * implement FluidHandlers
    * Finish Tank Access
    * move all the machines over to new tank system
    * trigger updates for MachineFluidHandler
    * change serialization keys and simply tank impl
    * fix serialization
    * reimpl interaction with Items with IFluidHandlerItem
    * delete old MachineFluidTank impl and renames
    * remove direct references of Tank index

--- src/machines/java/com/enderio/machines/common/blockentity/XPObeliskBlockEntity.java
+++ src/machines/java/com/enderio/machines/common/blockentity/XPObeliskBlockEntity.java
@@ -5,6 +5,10 @@ import com.enderio.base.common.tag.EIOTags;
 import com.enderio.base.common.util.ExperienceUtil;
 import com.enderio.core.common.network.slot.IntegerNetworkDataSlot;
 import com.enderio.machines.common.blockentity.base.MachineBlockEntity;
+import com.enderio.machines.common.io.fluid.MachineFluidHandler;
+import com.enderio.machines.common.io.fluid.MachineFluidTank;
+import com.enderio.machines.common.io.fluid.MachineTankLayout;
+import com.enderio.machines.common.io.fluid.TankAccess;
 import com.enderio.machines.common.menu.XPObeliskMenu;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Inventory;
@@ -15,18 +19,18 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.capability.IFluidHandler;
-import net.minecraftforge.fluids.capability.templates.FluidTank;
 import org.jetbrains.annotations.Nullable;
 
 public class XPObeliskBlockEntity extends MachineBlockEntity {
 
     IntegerNetworkDataSlot xpTankDataSlot;
+    private static final TankAccess TANK = new TankAccess();
 
     public XPObeliskBlockEntity(BlockEntityType<?> type, BlockPos worldPosition, BlockState blockState) {
         super(type, worldPosition, blockState);
 
-        this.xpTankDataSlot = new IntegerNetworkDataSlot(() -> getFluidTankNN().getFluidAmount(),
-            amount -> getFluidTankNN().setFluid(new FluidStack(EIOFluids.XP_JUICE.getSource(), amount)));
+        this.xpTankDataSlot = new IntegerNetworkDataSlot(() -> TANK.getFluidAmount(this),
+            amount -> TANK.setFluid(this, new FluidStack(EIOFluids.XP_JUICE.getSource(), amount)));
         addDataSlot(xpTankDataSlot);
     }
 
@@ -37,19 +41,24 @@ public class XPObeliskBlockEntity extends MachineBlockEntity {
     }
 
     @Override
-    protected @Nullable FluidTank createFluidTank() {
-        return new FluidTank(Integer.MAX_VALUE, fluidStack -> fluidStack.getFluid().is(EIOTags.Fluids.EXPERIENCE)) {
+    public @Nullable MachineTankLayout getTankLayout() {
+        return new MachineTankLayout.Builder().tank(TANK, Integer.MAX_VALUE, fluidStack -> fluidStack.getFluid().is(EIOTags.Fluids.EXPERIENCE)).build();
+    }
+
+    @Override
+    protected @Nullable MachineFluidHandler createFluidHandler(MachineTankLayout layout) {
+        return new MachineFluidHandler(getIOConfig(), getTankLayout()) {
             @Override
-            protected void onContentsChanged() {
-                super.onContentsChanged();
+            protected void onContentsChanged(int slot) {
+                super.onContentsChanged(slot);
                 setChanged();
             }
 
             @Override
             public int fill(FluidStack resource, FluidAction action) {
                 // Convert into XP Juice
-                if (this.isFluidValid(resource)) {
-                    var currentFluid = this.getFluid().getFluid();
+                if (TANK.isFluidValid(this, resource)) {
+                    var currentFluid = TANK.getFluid(this).getFluid();
                     if (currentFluid == Fluids.EMPTY || resource.getFluid().isSame(currentFluid)) {
                         return super.fill(resource, action);
                     } else {
@@ -63,12 +72,16 @@ public class XPObeliskBlockEntity extends MachineBlockEntity {
         };
     }
 
+    public MachineFluidTank getFluidTank() {
+        return TANK.getTank(this);
+    }
+
     public void addLevelToPlayer(int levelDiff, Player player) {
         int requestedLevel = player.experienceLevel + levelDiff;
         requestedLevel = Math.max(requestedLevel, 0);
         long playerXP = ExperienceUtil.getPlayerTotalXp(player);
         long requestedXP = ExperienceUtil.getTotalXpFromLevel(requestedLevel) - playerXP;
-        int storedXP = getFluidTankNN().getFluidAmount() / ExperienceUtil.EXP_TO_FLUID;
+        int storedXP = TANK.getFluidAmount(this) / ExperienceUtil.EXP_TO_FLUID;
 
         long awardXP = levelDiff > 0 ? Math.min(storedXP, requestedXP) : requestedXP;
         awardXP(awardXP, player);
@@ -77,7 +90,7 @@ public class XPObeliskBlockEntity extends MachineBlockEntity {
     public void addAllLevelToPlayer(boolean give, Player player) {
         long awardXP = 0;
         if (give) {
-            awardXP = getFluidTankNN().getFluidAmount() / ExperienceUtil.EXP_TO_FLUID;
+            awardXP = TANK.getFluidAmount(this) / ExperienceUtil.EXP_TO_FLUID;
         } else {
             awardXP = -ExperienceUtil.getPlayerTotalXp(player);
         }
@@ -89,14 +102,23 @@ public class XPObeliskBlockEntity extends MachineBlockEntity {
         // Positive -> Give levels to player ; Negative -> Take levels from player
         if (volumeToRemove > 0) {
             int cappedVolume = (int) Math.min(Integer.MAX_VALUE, volumeToRemove);
-            FluidStack drained = getFluidTankNN().drain(cappedVolume, IFluidHandler.FluidAction.EXECUTE);
+            FluidStack drained = TANK.drain(this, cappedVolume, IFluidHandler.FluidAction.EXECUTE);
             player.giveExperiencePoints(drained.getAmount() / ExperienceUtil.EXP_TO_FLUID);
 
         } else {
             int cappedVolume = (int) Math.min(Integer.MAX_VALUE, -volumeToRemove); // Invert
-            int filled = getFluidTankNN().fill(new FluidStack(EIOFluids.XP_JUICE.getSource(), cappedVolume), IFluidHandler.FluidAction.EXECUTE);
+            int filled = TANK.fill(this, new FluidStack(EIOFluids.XP_JUICE.getSource(), cappedVolume), IFluidHandler.FluidAction.EXECUTE);
             player.giveExperiencePoints(-1 * filled / ExperienceUtil.EXP_TO_FLUID); // Negative -> Take
         }
     }
 
 }
+
+
+
+
+
+
+
+
+
