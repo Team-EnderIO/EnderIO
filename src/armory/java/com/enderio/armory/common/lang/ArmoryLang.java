15c50c68e98661237b75a7e2aa1503fe5ea8fa2c
LetterN <24603524+LetterN@users.noreply.github.com>
2024-04-09 22:04:25 +0800

    sync lang utils

--- src/armory/java/com/enderio/armory/common/lang/ArmoryLang.java
+++ src/armory/java/com/enderio/armory/common/lang/ArmoryLang.java
@@ -5,62 +5,69 @@ import com.tterrag.registrate.Registrate;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
 
 public class ArmoryLang {
     private static final Registrate REGISTRATE = EnderIO.registrate();
 
     // region Dark Steel
 
-    public static final MutableComponent HEAD_DROP_CHANCE = REGISTRATE.addLang("info", EnderIO.loc("headchance"), "%s%% chance to drop a mob head");
-    public static final MutableComponent DURABILITY_AMOUNT = REGISTRATE.addLang("info", EnderIO.loc("durability.amount"), "Durability %s");
+    public static final MutableComponent HEAD_DROP_CHANCE = addTranslation("info", EnderIO.loc("headchance"), "%s%% chance to drop a mob head");
+    public static final MutableComponent DURABILITY_AMOUNT = addTranslation("info", EnderIO.loc("durability.amount"), "Durability %s");
 
-    public static final MutableComponent DS_UPGRADE_XP_COST = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.cost"), "Costs %s Levels");
-    public static final Component DS_UPGRADE_ACTIVATE = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.activate"), "Right Click to Activate");
-    public static final Component DS_UPGRADE_ITEM_NO_XP = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.no_xp"), "Not enough XP");
-    public static final Component DS_UPGRADE_AVAILABLE = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.available"), "Available Upgrades").withStyle(
+    public static final MutableComponent DS_UPGRADE_XP_COST = addTranslation("info", EnderIO.loc("darksteel.upgrade.cost"), "Costs %s Levels");
+    public static final Component DS_UPGRADE_ACTIVATE = addTranslation("info", EnderIO.loc("darksteel.upgrade.activate"), "Right Click to Activate");
+    public static final Component DS_UPGRADE_ITEM_NO_XP = addTranslation("info", EnderIO.loc("darksteel.upgrade.no_xp"), "Not enough XP");
+    public static final Component DS_UPGRADE_AVAILABLE = addTranslation("info", EnderIO.loc("darksteel.upgrade.available"), "Available Upgrades").withStyle(
         ChatFormatting.YELLOW);
 
-    public static final Component DS_UPGRADE_EMPOWERED_I = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.empowered_l1"), "Empowered");
-    public static final Component DS_UPGRADE_EMPOWERED_II = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.empowered_l2"), "Empowered II");
-    public static final Component DS_UPGRADE_EMPOWERED_III = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.empowered_l3"), "Empowered III");
-    public static final Component DS_UPGRADE_EMPOWERED_IV = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.empowered_l4"), "Empowered IV");
-    public static final Component DS_UPGRADE_EMPOWERED_DESCRIPTION = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.empowered.description"),
+    public static final Component DS_UPGRADE_EMPOWERED_I = addTranslation("info", EnderIO.loc("darksteel.upgrade.empowered_l1"), "Empowered");
+    public static final Component DS_UPGRADE_EMPOWERED_II = addTranslation("info", EnderIO.loc("darksteel.upgrade.empowered_l2"), "Empowered II");
+    public static final Component DS_UPGRADE_EMPOWERED_III = addTranslation("info", EnderIO.loc("darksteel.upgrade.empowered_l3"), "Empowered III");
+    public static final Component DS_UPGRADE_EMPOWERED_IV = addTranslation("info", EnderIO.loc("darksteel.upgrade.empowered_l4"), "Empowered IV");
+    public static final Component DS_UPGRADE_EMPOWERED_DESCRIPTION = addTranslation("info", EnderIO.loc("darksteel.upgrade.empowered.description"),
         "Infuse the steel with the power of Micro Infinity");
-    public static final MutableComponent DS_UPGRADE_EMPOWERED_STORAGE = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.empowered.storage"),
+    public static final MutableComponent DS_UPGRADE_EMPOWERED_STORAGE = addTranslation("info", EnderIO.loc("darksteel.upgrade.empowered.storage"),
         "Holds up to %s \u00B5I");
-    public static final MutableComponent DS_UPGRADE_EMPOWERED_DAMAGE_ABSORPTION = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.empowered.absorption"),
+    public static final MutableComponent DS_UPGRADE_EMPOWERED_DAMAGE_ABSORPTION = addTranslation("info", EnderIO.loc("darksteel.upgrade.empowered.absorption"),
         "%s%% damage absorbed by \u00B5I");
-    public static final MutableComponent DS_UPGRADE_EMPOWERED_EFFICIENCY = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.empowered.efficiency"),
+    public static final MutableComponent DS_UPGRADE_EMPOWERED_EFFICIENCY = addTranslation("info", EnderIO.loc("darksteel.upgrade.empowered.efficiency"),
         "Efficiency +%s when powered");
-    public static final MutableComponent DS_UPGRADE_EMPOWERED_OBSIDIAM_EFFICIENCY = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.empowered.obsidian.efficiency"),
+    public static final MutableComponent DS_UPGRADE_EMPOWERED_OBSIDIAM_EFFICIENCY = addTranslation("info", EnderIO.loc("darksteel.upgrade.empowered.obsidian.efficiency"),
         "Efficiency +%s when breaking obsidian");
 
-    public static final Component DS_UPGRADE_SPOON = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.spoon"), "Spoon");
-    public static final Component DS_UPGRADE_SPOON_DESCRIPTION = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.spoon.description"),
+    public static final Component DS_UPGRADE_SPOON = addTranslation("info", EnderIO.loc("darksteel.upgrade.spoon"), "Spoon");
+    public static final Component DS_UPGRADE_SPOON_DESCRIPTION = addTranslation("info", EnderIO.loc("darksteel.upgrade.spoon.description"),
         "Who needs a shovel when you have a spoon?");
 
-    public static final Component DS_UPGRADE_FORK = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.fork"), "Fork");
-    public static final Component DS_UPGRADE_FORK_DESCRIPTION = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.fork.description"),
+    public static final Component DS_UPGRADE_FORK = addTranslation("info", EnderIO.loc("darksteel.upgrade.fork"), "Fork");
+    public static final Component DS_UPGRADE_FORK_DESCRIPTION = addTranslation("info", EnderIO.loc("darksteel.upgrade.fork.description"),
         "Who needs a hoe when you have a fork?");
 
-    public static final Component DS_UPGRADE_DIRECT = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.direct"), "Direct");
-    public static final Component DS_UPGRADE_DIRECT_DESCRIPTION = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.direct.description"),
+    public static final Component DS_UPGRADE_DIRECT = addTranslation("info", EnderIO.loc("darksteel.upgrade.direct"), "Direct");
+    public static final Component DS_UPGRADE_DIRECT_DESCRIPTION = addTranslation("info", EnderIO.loc("darksteel.upgrade.direct.description"),
         "Teleports harvested items directly into your inventory");
 
-    public static final Component DS_UPGRADE_EXPLOSIVE_I = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.explosive_l1"), "Explosive I");
-    public static final Component DS_UPGRADE_EXPLOSIVE_II = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.explosive_l2"), "Explosive II");
-    public static final Component DS_UPGRADE_EXPLOSIVE_DESCRIPTION = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.explosive.description"),
+    public static final Component DS_UPGRADE_EXPLOSIVE_I = addTranslation("info", EnderIO.loc("darksteel.upgrade.explosive_l1"), "Explosive I");
+    public static final Component DS_UPGRADE_EXPLOSIVE_II = addTranslation("info", EnderIO.loc("darksteel.upgrade.explosive_l2"), "Explosive II");
+    public static final Component DS_UPGRADE_EXPLOSIVE_DESCRIPTION = addTranslation("info", EnderIO.loc("darksteel.upgrade.explosive.description"),
         "Makes surrounding dirt and rock go splodey");
 
-    public static final Component DS_UPGRADE_EXPLOSIVE_PENETRATION_I = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.explosive_penetration_l1"),
+    public static final Component DS_UPGRADE_EXPLOSIVE_PENETRATION_I = addTranslation("info", EnderIO.loc("darksteel.upgrade.explosive_penetration_l1"),
         "Explosive Penetration I");
-    public static final Component DS_UPGRADE_EXPLOSIVE_PENETRATION_II = REGISTRATE.addLang("info", EnderIO.loc("darksteel.upgrade.explosive_penetration_l2"),
+    public static final Component DS_UPGRADE_EXPLOSIVE_PENETRATION_II = addTranslation("info", EnderIO.loc("darksteel.upgrade.explosive_penetration_l2"),
         "Explosive Penetration II");
-    public static final Component DS_UPGRADE_EXPLOSIVE_PENETRATION_DESCRIPTION = REGISTRATE.addLang("info",
+    public static final Component DS_UPGRADE_EXPLOSIVE_PENETRATION_DESCRIPTION = addTranslation("info",
         EnderIO.loc("darksteel.upgrade.explosive_penetration.description"), "Makes dirt and rock behind the mined block go splodey");
 
     // endregion
-
-    public static void register() {
+    private static MutableComponent addTranslation(String prefix, ResourceLocation id, String translation) {
+        return EnderIO.registrate().addLang(prefix, id, translation);
     }
+
+    private static MutableComponent addTranslation(String prefix, ResourceLocation path, String name, String translation) {
+        return EnderIO.registrate().addLang(prefix, new ResourceLocation(path.getNamespace(), path.getPath() + "/" + name), translation);
+    }
+
+    public static void register() {}
 }
--- src/conduits/java/com/enderio/conduits/common/init/ConduitLang.java
+++ src/conduits/java/com/enderio/conduits/common/init/ConduitLang.java
@@ -3,12 +3,22 @@ package com.enderio.conduits.common.init;
 import com.enderio.EnderIO;
 import com.tterrag.registrate.Registrate;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
 
 public class ConduitLang {
     private static final Registrate REGISTRATE = EnderIO.registrate();
 
-    public static final Component CONDUIT_INSERT = REGISTRATE.addLang("gui", EnderIO.loc("conduit.insert"), "Insert");
-    public static final Component CONDUIT_EXTRACT = REGISTRATE.addLang("gui", EnderIO.loc("conduit.extract"), "Extract");
+    public static final Component CONDUIT_INSERT = addTranslation("gui", EnderIO.loc("conduit.insert"), "Insert");
+    public static final Component CONDUIT_EXTRACT = addTranslation("gui", EnderIO.loc("conduit.extract"), "Extract");
+
+    private static MutableComponent addTranslation(String prefix, ResourceLocation id, String translation) {
+        return EnderIO.registrate().addLang(prefix, id, translation);
+    }
+
+    private static MutableComponent addTranslation(String prefix, ResourceLocation path, String name, String translation) {
+        return EnderIO.registrate().addLang(prefix, new ResourceLocation(path.getNamespace(), path.getPath() + "/" + name), translation);
+    }
 
     public static void register() {
     }
