fa21fa5279c838e165a6d53945d572427d4bc2f4
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2024-05-10 23:45:59 +0100

    20.6: I don't think using attachments for paint makes sense at this time.

--- src/main/java/com/enderio/base/common/init/EIOAttachments.java
+++ src/main/java/com/enderio/base/common/init/EIOAttachments.java
@@ -12,20 +12,7 @@ import java.util.function.Supplier;
 
 public class EIOAttachments {
     private static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES = DeferredRegister.create(NeoForgeRegistries.Keys.ATTACHMENT_TYPES, EnderIO.MODID);
-
-    // TODO: Do we use these or nah?
-    public static final Supplier<AttachmentType<BlockPaintData>> PRIMARY_PAINT =
-        ATTACHMENT_TYPES.register("primary_paint", () ->
-            AttachmentType.builder(() -> BlockPaintData.of(Blocks.AIR))
-                .serialize(BlockPaintData.CODEC)
-                .build());
-
-    public static final Supplier<AttachmentType<BlockPaintData>> SECONDARY_PAINT =
-        ATTACHMENT_TYPES.register("secondary_paint", () ->
-            AttachmentType.builder(() -> BlockPaintData.of(Blocks.AIR))
-                .serialize(BlockPaintData.CODEC)
-                .build());
-
+    
     public static void register(IEventBus bus) {
         ATTACHMENT_TYPES.register(bus);
     }
