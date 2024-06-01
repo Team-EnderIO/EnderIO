152c03440e9beb341680a3284a04ba705e74246f
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2024-06-02 00:37:15 +0200

    fix: Capacitors clearing stored energy when removed

--- src/machines/java/com/enderio/machines/common/io/energy/MachineEnergyStorage.java
+++ src/machines/java/com/enderio/machines/common/io/energy/MachineEnergyStorage.java
@@ -124,7 +124,7 @@ public class MachineEnergyStorage implements IMachineEnergyStorage, IEnderCapabi
 
     @Override
     public int receiveEnergy(int maxReceive, boolean simulate) {
-        if (!canReceive()) {
+        if (!canReceive() || getMaxEnergyStored() == 0) {
             return 0;
         }
 
