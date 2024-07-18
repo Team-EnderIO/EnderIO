87eb5293c7291c149ead6ea461d1feceff697e37
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2024-07-18 22:43:14 +0100

    feat: Implemented ChunkBoundLookup and use it to handle Obelisk Spawn & Teleport Events.

--- enderio-machines/src/main/java/com/enderio/machines/common/init/MachineAttachments.java
+++ enderio-machines/src/main/java/com/enderio/machines/common/init/MachineAttachments.java
@@ -3,7 +3,11 @@ package com.enderio.machines.common.init;
 import com.enderio.EnderIOBase;
 import com.enderio.base.api.misc.RedstoneControl;
 import com.enderio.machines.common.attachment.ActionRange;
+import com.enderio.machines.common.blockentity.AversionObeliskBlockEntity;
 import com.enderio.machines.common.io.IOConfig;
+import com.enderio.machines.common.obelisk.AversionObeliskManager;
+import com.enderio.machines.common.obelisk.InhibitorObeliskManager;
+import com.enderio.machines.common.obelisk.RelocatorObeliskManager;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.attachment.AttachmentType;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -26,6 +30,15 @@ public class MachineAttachments {
 
     public static final Supplier<AttachmentType<IOConfig>> IO_CONFIG
         = ATTACHMENT_TYPES.register("io_config", () -> AttachmentType.builder(IOConfig::empty).serialize(IOConfig.CODEC).build());
+
+    public static final Supplier<AttachmentType<AversionObeliskManager>> AVERSION_OBELISK_MANAGER
+        = ATTACHMENT_TYPES.register("aversion_obelisk_manager", () -> AttachmentType.builder(AversionObeliskManager::new).build());
+
+    public static final Supplier<AttachmentType<InhibitorObeliskManager>> INHIBITOR_OBELISK_MANAGER
+        = ATTACHMENT_TYPES.register("inhibitor_obelisk_manager", () -> AttachmentType.builder(InhibitorObeliskManager::new).build());
+
+    public static final Supplier<AttachmentType<RelocatorObeliskManager>> RELOCATOR_OBELISK_MANAGER
+        = ATTACHMENT_TYPES.register("relocator_obelisk_manager", () -> AttachmentType.builder(RelocatorObeliskManager::new).build());
 
     public static void register(IEventBus bus) {
         ATTACHMENT_TYPES.register(bus);
