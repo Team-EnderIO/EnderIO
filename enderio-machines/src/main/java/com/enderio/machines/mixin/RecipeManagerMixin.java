3f1b6d0008406875d8dbb532a5180229fb38cac1
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2024-09-28 16:57:58 +0100

    fix: Skip smelting recipes that do not have valid result items.
    Writes a message to the log to help devs catch recipe problems.

--- enderio-machines/src/main/java/com/enderio/machines/mixin/RecipeManagerMixin.java
+++ enderio-machines/src/main/java/com/enderio/machines/mixin/RecipeManagerMixin.java
@@ -7,6 +7,8 @@ import com.enderio.machines.common.recipe.AlloySmeltingRecipe;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMultimap;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.RecipeManager;
@@ -21,6 +23,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mixin(RecipeManager.class)
 public class RecipeManagerMixin {
@@ -29,21 +32,30 @@ public class RecipeManagerMixin {
     private static void collectRecipe(ResourceLocation recipeId, ImmutableMultimap.Builder<RecipeType<?>, RecipeHolder<?>> byType,
         ImmutableMap.Builder<ResourceLocation, RecipeHolder<?>> byName, WithConditions<Recipe<?>> recipeWithConditions, CallbackInfo ci) {
         if (recipeWithConditions.carrier() instanceof SmeltingRecipe smeltingRecipe) {
-            RecipeHolder<AlloySmeltingRecipe> convertedHolder = enderio$convert(recipeId, smeltingRecipe);
-            byType.put(MachineRecipes.ALLOY_SMELTING.type().get(), convertedHolder);
-            byName.put(convertedHolder.id(), convertedHolder);
+            Optional<RecipeHolder<AlloySmeltingRecipe>> convertedHolder = enderio$convert(recipeId, smeltingRecipe);
+
+            convertedHolder.ifPresent(holder -> {
+                byType.put(MachineRecipes.ALLOY_SMELTING.type().get(), holder);
+                byName.put(holder.id(), holder);
+            });
         }
     }
 
     @Unique
-    private static RecipeHolder<AlloySmeltingRecipe> enderio$convert(ResourceLocation originalId, SmeltingRecipe smeltingRecipe) {
+    private static Optional<RecipeHolder<AlloySmeltingRecipe>> enderio$convert(ResourceLocation originalId, SmeltingRecipe smeltingRecipe) {
         AbstractCookingRecipeAccessor accessor = (AbstractCookingRecipeAccessor) smeltingRecipe;
+
+        if (accessor.getResult().isEmpty()) {
+            EnderIOBase.LOGGER.warn("Unable to inherit the cooking recipe with ID: {}. Reason: The result item is empty.", originalId);
+            return Optional.empty();
+        }
+
         SizedIngredient input = new SizedIngredient(accessor.getIngredient(), 1);
         int energy = MachinesConfig.COMMON.ENERGY.ALLOY_SMELTER_VANILLA_ITEM_ENERGY.get();
         AlloySmeltingRecipe recipe = new AlloySmeltingRecipe(List.of(input), accessor.getResult(), energy, accessor.getExperience(), true);
 
         String path = "smelting/" + originalId.getNamespace() + "/" + originalId.getPath();
         ResourceLocation id = EnderIOBase.loc(path);
-        return new RecipeHolder<>(id, recipe);
+        return Optional.of(new RecipeHolder<>(id, recipe));
     }
 }
