5befbc92200b76f0e8cb2cfa0d537e6e80cbadaf
OE100 <omer.er10@gmail.com>
2024-10-28 07:58:56 +0200

    added event listeners for player input related to the probe

---
+++ src/conduits/java/com/enderio/conduits/client/input/InputEventListener.java
@@ -0,0 +1,53 @@
+package com.enderio.conduits.client.input;
+
+import com.enderio.EnderIO;
+import com.enderio.base.client.input.KeyBindings;
+import com.enderio.conduits.common.items.tool.ConduitProbeItem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@OnlyIn(Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = EnderIO.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class InputEventListener {
+    @SubscribeEvent
+    public static void inputEvent(InputEvent.MouseScrollingEvent event) {
+        LocalPlayer player = Minecraft.getInstance().player;
+        if (player == null) return;
+        
+        ItemStack heldMainHand = player.getMainHandItem();
+        ItemStack heldOffHand = player.getOffhandItem();
+        
+        if (Math.abs(event.getScrollDelta()) > 0 && player.isCrouching()) {
+            boolean cancelScroll = false;
+            if (heldMainHand.getItem() instanceof ConduitProbeItem probeItem) {
+                probeItem.switchState();
+                cancelScroll = true;
+            }
+            if (heldOffHand.getItem() instanceof ConduitProbeItem probeItem) {
+                probeItem.switchState();
+                cancelScroll = true;
+            }
+            event.setCanceled(cancelScroll);
+        }
+    }
+    
+    @SubscribeEvent
+    public static void inputEvent(InputEvent.Key event) {
+        LocalPlayer player = Minecraft.getInstance().player;
+        if (player == null) return;
+
+        ItemStack heldMainHand = player.getMainHandItem();
+        ItemStack heldOffHand = player.getOffhandItem();
+        
+        if (KeyBindings.MODE_CHANGE.consumeClick()) {
+            if (heldMainHand.getItem() instanceof ConduitProbeItem probeItem) probeItem.switchState();
+            if (heldOffHand.getItem() instanceof ConduitProbeItem probeItem) probeItem.switchState();
+        }
+    }
+}
