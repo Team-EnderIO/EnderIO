aba594bae0ddb9fa25e7f5989dd58b8d5d2cabb3
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2024-12-29 21:45:19 +0000

    fix: Facade visibility check doesn't work when switching yeta from offhand to main.

--- enderio-conduits/src/main/java/com/enderio/conduits/client/model/conduit/facades/FacadeVisibilityHandler.java
+++ enderio-conduits/src/main/java/com/enderio/conduits/client/model/conduit/facades/FacadeVisibilityHandler.java
@@ -4,13 +4,14 @@ import com.enderio.base.common.tag.EIOTags;
 import com.enderio.conduits.EnderIOConduits;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingEquipmentChangeEvent;
 
 @EventBusSubscriber(modid = EnderIOConduits.MODULE_MOD_ID, bus = EventBusSubscriber.Bus.GAME, value = Dist.CLIENT)
-public class YetaChunkRebuildHandler {
+public class FacadeVisibilityHandler {
     @SubscribeEvent
     public static void onEquipmentChanged(LivingEquipmentChangeEvent event) {
         if (event.getSlot() != EquipmentSlot.MAINHAND &&
@@ -21,7 +22,13 @@ public class YetaChunkRebuildHandler {
 
         if (event.getTo().is(EIOTags.Items.HIDE_FACADES) ||
             event.getFrom().is(EIOTags.Items.HIDE_FACADES)) {
-            FacadeHelper.setFacadesVisible(!event.getTo().is(EIOTags.Items.HIDE_FACADES));
+
+            // Check both hands directly in case of an offhand swap.
+            ItemStack mainHand = event.getEntity().getItemBySlot(EquipmentSlot.MAINHAND);
+            ItemStack offHand = event.getEntity().getItemBySlot(EquipmentSlot.OFFHAND);
+            FacadeHelper.setFacadesVisible(!mainHand.is(EIOTags.Items.HIDE_FACADES) &&
+                !offHand.is(EIOTags.Items.HIDE_FACADES));
+
             FacadeHelper.rebuildChunkMeshes();
         }
     }
