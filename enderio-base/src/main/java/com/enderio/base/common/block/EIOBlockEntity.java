49c3db8b114bef2c38db06a94ffc8f3ad2d4206e
CrazyPants <crazypants.mc@gmail.com>
2025-02-13 14:57:33 +0800

    If a machine is set to only active with redstone this is ignored when a world is reloaded.

--- enderio-base/src/main/java/com/enderio/base/common/block/EIOBlockEntity.java
+++ enderio-base/src/main/java/com/enderio/base/common/block/EIOBlockEntity.java
@@ -27,7 +27,7 @@ public class EIOBlockEntity extends EnderBlockEntity {
 
         if (level != null && !level.isClientSide()) {
             if (isRedstoneDirty) {
-                updateRedstonePower();
+                updateRedstonePower(false);
             }
         }
     }
@@ -37,7 +37,7 @@ public class EIOBlockEntity extends EnderBlockEntity {
         super.onLoad();
 
         if (supportsRedstonePower()) {
-            updateRedstonePower();
+            updateRedstonePower(true);
         }
     }
 
@@ -60,13 +60,13 @@ public class EIOBlockEntity extends EnderBlockEntity {
         return isRedstonePowered;
     }
 
-    private void updateRedstonePower() {
+    private void updateRedstonePower(boolean forceUpdate) {
         boolean hasPower = level.hasNeighborSignal(worldPosition);
         boolean didChange = isRedstonePowered != hasPower;
         isRedstonePowered = hasPower;
         isRedstoneDirty = false;
 
-        if (didChange) {
+        if (didChange || forceUpdate) {
             onRedstonePowerChanged();
         }
     }
