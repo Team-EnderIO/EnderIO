6c87cdc787418da0cce9fed8904fbc433a113ecd
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2025-03-15 16:03:11 +0100

    feat: Recipes to erase machine data
    + fix powered spawner not showing in jei

--- enderio-machines/src/main/java/com/enderio/machines/data/recipes/MachineRecipeProvider.java
+++ enderio-machines/src/main/java/com/enderio/machines/data/recipes/MachineRecipeProvider.java
@@ -5,9 +5,13 @@ import com.enderio.base.common.init.EIOBlocks;
 import com.enderio.base.common.init.EIOItems;
 import com.enderio.base.common.tag.EIOTags;
 import com.enderio.base.data.recipe.ShapedEntityStorageRecipeBuilder;
+import com.enderio.machines.EnderIOMachines;
 import com.enderio.machines.common.blockentity.capacitorbank.CapacitorTier;
 import com.enderio.machines.common.blockentity.solar.SolarPanelTier;
+import com.enderio.machines.common.blocks.base.block.MachineBlock;
 import com.enderio.machines.common.init.MachineBlocks;
+import com.enderio.regilite.holder.RegiliteBlock;
+import net.minecraft.Util;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.core.HolderLookup;
@@ -17,11 +21,14 @@ import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.common.Tags;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class MachineRecipeProvider extends RecipeProvider {
@@ -420,5 +427,46 @@ public class MachineRecipeProvider extends RecipeProvider {
             .pattern("NRN")
             .unlockedBy("has_ingredient", InventoryChangeTrigger.TriggerInstance.hasItems(ItemPredicate.Builder.item().of(Items.PISTON).build()))
             .save(recipeOutput);*/
+
+        eraseRecipes(recipeOutput);
+    }
+
+    private static final List<RegiliteBlock<?>> MACHINES = Util.make(() -> {
+        List<RegiliteBlock<?>> list = new ArrayList<>();
+        list.addAll(MachineBlocks.CAPACITOR_BANKS.values());
+        list.add(MachineBlocks.FLUID_TANK);
+        list.add(MachineBlocks.PRESSURIZED_FLUID_TANK);
+        list.add(MachineBlocks.PRIMITIVE_ALLOY_SMELTER);
+        list.add(MachineBlocks.ALLOY_SMELTER);
+        list.add(MachineBlocks.STIRLING_GENERATOR);
+        list.add(MachineBlocks.SAG_MILL);
+        list.add(MachineBlocks.SLICE_AND_SPLICE);
+        list.add(MachineBlocks.SOUL_BINDER);
+        list.add(MachineBlocks.WIRED_CHARGER);
+        list.add(MachineBlocks.POWERED_SPAWNER);
+        list.add(MachineBlocks.SOUL_ENGINE);
+        list.add(MachineBlocks.DRAIN);
+        list.add(MachineBlocks.CRAFTER);
+        list.addAll(MachineBlocks.SOLAR_PANELS.values());
+        list.add(MachineBlocks.PAINTING_MACHINE);
+        list.add(MachineBlocks.IMPULSE_HOPPER);
+        list.add(MachineBlocks.XP_OBELISK);
+        list.add(MachineBlocks.AVERSION_OBELISK);
+        list.add(MachineBlocks.INHIBITOR_OBELISK);
+        list.add(MachineBlocks.RELOCATOR_OBELISK);
+        list.add(MachineBlocks.XP_VACUUM);
+        list.add(MachineBlocks.VACUUM_CHEST);
+        list.add(MachineBlocks.TRAVEL_ANCHOR);
+        return list;
+    });
+
+    public void eraseRecipes(RecipeOutput recipeOutput) {
+        for (var block : MACHINES) {
+            ShapelessRecipeBuilder
+                .shapeless(RecipeCategory.MISC, block)
+                .requires(block)
+                .unlockedBy("has_ingredient", InventoryChangeTrigger.TriggerInstance.hasItems(block))
+                .save(recipeOutput, EnderIO.loc("erase_" + block.getId().getPath()));
+        }
     }
 }
