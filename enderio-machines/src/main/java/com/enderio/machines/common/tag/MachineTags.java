6bc3ec343a3a2c85adbaa6a358c9f53f5e11ec50
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2025-05-06 23:04:46 +0100

    feat: VAT recipes
    Co-authored-by: ferriarnus <61201275+ferriarnus@users.noreply.github.com>
    Ref: GH-1061

--- enderio-machines/src/main/java/com/enderio/machines/common/tag/MachineTags.java
+++ enderio-machines/src/main/java/com/enderio/machines/common/tag/MachineTags.java
@@ -4,13 +4,38 @@ import com.enderio.base.api.EnderIO;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class MachineTags {
 
     public static void register() {
+        ItemTags.init();
         EntityTypes.init();
         Blocks.init();
+    }
+
+    public static class ItemTags {
+
+        private static void init() {
+        }
+
+        public static TagKey<Item> SEEDS = create("seeds");
+        public static TagKey<Item> CROPS = create("crops");
+        public static TagKey<Item> MEAT = create("meat");
+        public static TagKey<Item> EXPLOSIVES = create("explosives");
+        public static TagKey<Item> BLAZE_POWDER = create("blaze_powder");
+        public static final TagKey<Item> NATURAL_LIGHTS = create("natural_lights");
+        public static final TagKey<Item> SUNFLOWER = create("sunflower");
+        public static final TagKey<Item> AMETHYST = create("amethyst");
+        public static final TagKey<Item> CLOUD_COLD = create("cloud_cold");
+        public static final TagKey<Item> PRISMARINE = create("prismarine");
+        public static final TagKey<Item> LIGHTNING_ROD = create("lightning_rod");
+        public static final TagKey<Item> WIND_CHARGES = create("wind_charges");
+
+        private static TagKey<Item> create(String pName) {
+            return TagKey.create(Registries.ITEM, EnderIO.loc(pName));
+        }
     }
 
     public static class EntityTypes {
