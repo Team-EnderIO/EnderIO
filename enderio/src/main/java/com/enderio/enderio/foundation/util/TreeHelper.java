2551d41f8c0286906cc3290ffe985ddab33f2286
Darshan Phaldesai <dev.darshanphaldesai@gmail.com>
2025-10-07 21:13:50 -0700

    farming-station: redo tree algorithm
    Copied from Ars Nouveau. Bailey you are a real mage

--- enderio/src/main/java/com/enderio/enderio/foundation/util/TreeHelper.java
+++ enderio/src/main/java/com/enderio/enderio/foundation/util/TreeHelper.java
@@ -5,8 +5,8 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.ArrayDeque;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.Set;
 import java.util.function.Predicate;
 
@@ -17,23 +17,26 @@ public class TreeHelper {
     }
 
     public static Set<BlockPos> getTree(Level level, BlockPos bottom, Predicate<BlockPos> inRange) {
-        LinkedList<BlockPos> candidates = new LinkedList<>();
+        ArrayDeque<BlockPos> queue = new ArrayDeque<>();
         HashSet<BlockPos> tree = new HashSet<>();
         HashSet<BlockPos> seen = new HashSet<>();
 
-        candidates.add(bottom);
+        queue.add(bottom);
 
-        while (!candidates.isEmpty()) {
-            BlockPos pos = candidates.removeFirst();
+        while (!queue.isEmpty()) {
+            BlockPos pos = queue.removeFirst();
             BlockState state = level.getBlockState(pos);
-            seen.add(pos);
-            if (isTree(state) && inRange.test(pos)) {
+
+            if(isTree(state) && inRange.test(pos)) {
                 tree.add(pos);
-                BlockPos.betweenClosed(pos.offset(1, 1, 1), pos.offset(-1, -1, -1)).forEach(next -> {
-                    if (seen.contains(next))
-                        return;
-                    candidates.add(next);
-                });
+                BlockPos.betweenClosedStream(pos.offset(-1, -1, -1), pos.offset(1, 1, 1))
+                    .forEach( next ->  {
+                        if (seen.contains(next)) return;
+
+                        var immutable = next.immutable();
+                        seen.add(immutable);
+                        queue.add(immutable);
+                    });
             }
         }
         return tree;
