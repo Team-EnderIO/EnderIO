19fa3463b658b16242abd5963ba2ac33c36376e2
overkidding <46853321+overkidding@users.noreply.github.com>
2025-11-11 02:05:07 +0100

    fix: add some tick buffering

--- endercore/src/main/java/com/enderio/core/common/blockentity/EnderBlockEntity.java
+++ endercore/src/main/java/com/enderio/core/common/blockentity/EnderBlockEntity.java
@@ -62,6 +62,8 @@ public class EnderBlockEntity extends BlockEntity {
         blockEntity.endTick();
     }
 
+    private int nextTick = 0;
+
     /**
      * Perform server-side ticking
      */
@@ -69,7 +71,10 @@ public class EnderBlockEntity extends BlockEntity {
     public void serverTick() {
         // Perform syncing.
         if (level != null) {
-            sync();
+            if(nextTick-- == 0) { // do we really need to update it once a tick?
+                sync();
+                nextTick = 5;
+            }
         }
     }
 
