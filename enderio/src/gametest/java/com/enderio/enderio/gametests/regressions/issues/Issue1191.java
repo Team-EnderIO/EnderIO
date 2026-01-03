fad146640833a27f7ddf58f347e4e4541ce16b0b
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2026-01-03 01:29:40 +0000

    fix: Fluid tank duplicating fluid and add regression test
    Closes: GH-1191

---
+++ enderio/src/gametest/java/com/enderio/enderio/gametests/regressions/issues/Issue1191.java
@@ -0,0 +1,44 @@
+package com.enderio.enderio.gametests.regressions.issues;
+
+import com.enderio.enderio.api.io.IOMode;
+import com.enderio.enderio.gametests.util.EnderGameTestHelper;
+import com.enderio.enderio.init.EIOBlocks;
+import com.enderio.enderio.init.EIOFluids;
+import net.minecraft.core.Direction;
+import net.minecraft.gametest.framework.GameTest;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
+import net.neoforged.testframework.DynamicTest;
+import net.neoforged.testframework.annotation.ForEachTest;
+import net.neoforged.testframework.annotation.TestHolder;
+import net.neoforged.testframework.gametest.StructureTemplateBuilder;
+
+/**
+ * Regression tests for issue #1191 - Fluid tank infinitely produces / dupes xp juice from two Bottles o' Enchanting.
+ * https://github.com/Team-EnderIO/EnderIO/issues/1191
+ */
+@ForEachTest(groups = "regression.issue1191")
+public class Issue1191 {
+    @GameTest
+    @TestHolder(description = "Ensures that placing conduits forms a network and breaking the middle splits the network.")
+    public static void testIssue1191(final DynamicTest test) {
+        test.registerGameTestTemplate(() -> StructureTemplateBuilder.withSize(1, 1, 3)
+            .set(0, 0, 0, Blocks.CHEST.defaultBlockState())
+            .set(0, 0, 1, EIOBlocks.FLUID_TANK.get().defaultBlockState())
+            .set(0, 0, 2, EIOBlocks.XP_OBELISK.get().defaultBlockState()));
+
+        test.onGameTest(EnderGameTestHelper.class, helper -> {
+            helper.startSequence()
+                // Setup fluid tank as described by the issue
+                .thenExecute(() -> helper.changeIoConfig(0, 1, 1, ioConfigurable -> {
+                    ioConfigurable.setIOMode(Direction.NORTH, IOMode.BOTH);
+                    ioConfigurable.setIOMode(Direction.SOUTH, IOMode.PUSH);
+                }))
+                // Insert 2 Bottles o' Enchanting into the tank
+                .thenExecute(() -> helper.insertIntoContainer(0, 1, 1, Items.EXPERIENCE_BOTTLE, 2))
+                // Make sure we have exactly 2 Bottles o' Enchanting worth of xp juice in the tank after 5 seconds
+                .thenExecuteAfter(20 * 5, () -> helper.assertContainerHasExactly(0, 1, 2, EIOFluids.XP_JUICE.source().get(), 500))
+                .thenSucceed();
+        });
+    }
+}
---
+++ enderio/src/gametest/java/com/enderio/enderio/gametests/util/EnderGameTestHelper.java
@@ -0,0 +1,101 @@
+package com.enderio.enderio.gametests.util;
+
+import com.enderio.enderio.api.io.IOConfigurable;
+import net.minecraft.core.BlockPos;
+import net.minecraft.gametest.framework.GameTestAssertException;
+import net.minecraft.gametest.framework.GameTestInfo;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.material.Fluid;
+import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
+import net.neoforged.testframework.gametest.ExtendedGameTestHelper;
+
+import java.util.function.Consumer;
+
+public class EnderGameTestHelper extends ExtendedGameTestHelper {
+    public EnderGameTestHelper(GameTestInfo info) {
+        super(info);
+    }
+
+    public void changeIoConfig(int x, int y, int z, Consumer<IOConfigurable> configurer) {
+        var blockEntity = getLevel().getBlockEntity(absolutePos(new BlockPos(x, y, z)));
+        if (blockEntity instanceof IOConfigurable ioConfigurable) {
+            configurer.accept(ioConfigurable);
+        } else {
+            throw new GameTestAssertException("No IOConfigurable at " + x + "," + y + "," + z);
+        }
+    }
+
+    public void insertIntoContainer(int x, int y, int z, Item item, int count) {
+        var itemHandler = getLevel().getCapability(Capabilities.ItemHandler.BLOCK, absolutePos(new BlockPos(x, y, z)),
+            null);
+        if (itemHandler == null) {
+            throw new GameTestAssertException("No item handler at " + x + "," + y + "," + z);
+        }
+
+        for (int i = 0; i < itemHandler.getSlots(); i++) {
+            int toInsert = Math.min(count, itemHandler.getSlotLimit(i));
+            var remainder = itemHandler.insertItem(i, new ItemStack(item, toInsert), false);
+            count -= toInsert - remainder.getCount();
+            if (count <= 0) {
+                return;
+            }
+        }
+
+        if (count > 0) {
+            throw new GameTestAssertException(
+                "Could not insert " + count + " items into container at " + x + "," + y + "," + z);
+        }
+    }
+
+    public void assertContainerHasExactly(int x, int y, int z, Item item, int count) {
+        var itemHandler = getLevel().getCapability(Capabilities.ItemHandler.BLOCK, absolutePos(new BlockPos(x, y, z)),
+            null);
+        if (itemHandler == null) {
+            throw new GameTestAssertException("No item handler at " + x + "," + y + "," + z);
+        }
+
+        int foundCount = 0;
+        for (int i = 0; i < itemHandler.getSlots(); i++) {
+            var available = itemHandler.extractItem(i, Math.min(count, itemHandler.getSlotLimit(i)), true);
+            foundCount += available.getCount();
+        }
+
+        if (foundCount != count) {
+            throw new GameTestAssertException("Expected " + count + " of " + item + " in container at " + x + "," + y
+                + "," + z + " but found " + foundCount);
+        }
+    }
+
+    public void fillContainer(int x, int y, int z, Fluid fluid, int amount) {
+        var fluidHandler = getLevel().getCapability(Capabilities.FluidHandler.BLOCK, absolutePos(new BlockPos(x, y, z)),
+            null);
+        if (fluidHandler == null) {
+            throw new GameTestAssertException("No fluid handler at " + x + "," + y + "," + z);
+        }
+
+        int filled = fluidHandler.fill(new FluidStack(fluid, amount), IFluidHandler.FluidAction.EXECUTE);
+
+        if (filled < amount) {
+            throw new GameTestAssertException(
+                "Could not fill tank with all " + amount + " of the fluid into container at " + x + "," + y + "," + z);
+        }
+    }
+
+    public void assertContainerHasExactly(int x, int y, int z, Fluid fluid, int amount) {
+        var fluidHandler = getLevel().getCapability(Capabilities.FluidHandler.BLOCK, absolutePos(new BlockPos(x, y, z)),
+            null);
+        if (fluidHandler == null) {
+            throw new GameTestAssertException("No fluid handler at " + x + "," + y + "," + z);
+        }
+
+        int foundAmount = fluidHandler.drain(new FluidStack(fluid, Integer.MAX_VALUE), IFluidHandler.FluidAction.SIMULATE).getAmount();
+
+        if (foundAmount != amount) {
+            throw new GameTestAssertException("Expected " + amount + " of " + fluid + " in tank at " + x + "," + y
+                + "," + z + " but found " + foundAmount);
+        }
+    }
+}
