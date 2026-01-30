e36c334815dc7d1accd4c13429b533fe3b2da1b6
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2026-01-30 21:48:27 +0100

    fix: Soul Filter Matching Logic Flaw

--- enderio/src/test/java/com/enderio/enderio/tests/filters/EnderSoulFilterTests.java
+++ enderio/src/test/java/com/enderio/enderio/tests/filters/EnderSoulFilterTests.java
@@ -11,18 +11,20 @@ import org.junit.jupiter.api.Test;
 public class EnderSoulFilterTests {
     @Test
     public void testBasicAllowFilter() {
-        var filter = new EnderSoulFilter(NonNullList.of(Soul.EMPTY, Soul.of(EntityType.ALLAY)), false, false);
+        var filter = new EnderSoulFilter(NonNullList.of(Soul.EMPTY, Soul.of(EntityType.ALLAY), Soul.of(EntityType.SKELETON)), false, false);
 
         Assertions.assertTrue(filter.test(Soul.of(EntityType.ALLAY)));
+        Assertions.assertTrue(filter.test(Soul.of(EntityType.SKELETON)));
         Assertions.assertFalse(filter.test(Soul.of(EntityType.COD)));
         Assertions.assertFalse(filter.test(Soul.of(EntityType.SHEEP)));
     }
 
     @Test
     public void testBasicDenyFilter() {
-        var filter = new EnderSoulFilter(NonNullList.of(Soul.EMPTY, Soul.of(EntityType.ALLAY)), true, false);
+        var filter = new EnderSoulFilter(NonNullList.of(Soul.EMPTY, Soul.of(EntityType.ALLAY), Soul.of(EntityType.SKELETON)), true, false);
 
         Assertions.assertFalse(filter.test(Soul.of(EntityType.ALLAY)));
+        Assertions.assertFalse(filter.test(Soul.of(EntityType.SKELETON)));
         Assertions.assertTrue(filter.test(Soul.of(EntityType.COD)));
         Assertions.assertTrue(filter.test(Soul.of(EntityType.SHEEP)));
     }
@@ -32,12 +34,32 @@ public class EnderSoulFilterTests {
         CompoundTag tag = new CompoundTag();
         tag.putInt("Health", 20);
         var soulWithHealth = new Soul(EntityType.ALLAY, tag);
+        var anotherSoulWithHealth = new Soul(EntityType.SKELETON, tag);
 
-        var filter = new EnderSoulFilter(NonNullList.of(Soul.EMPTY, soulWithHealth), false, true);
+        var filter = new EnderSoulFilter(NonNullList.of(Soul.EMPTY, soulWithHealth, anotherSoulWithHealth), false, true);
 
         Assertions.assertTrue(filter.test(soulWithHealth));
         Assertions.assertFalse(filter.test(Soul.of(EntityType.ALLAY)));
+        Assertions.assertTrue(filter.test(anotherSoulWithHealth));
+        Assertions.assertFalse(filter.test(Soul.of(EntityType.SKELETON)));
         Assertions.assertFalse(filter.test(Soul.of(EntityType.COD)));
         Assertions.assertFalse(filter.test(Soul.of(EntityType.SHEEP)));
+    }
+
+    @Test
+    public void testBasicDenyFilterWithComponentComparison() {
+        CompoundTag tag = new CompoundTag();
+        tag.putInt("Health", 20);
+        var soulWithHealth = new Soul(EntityType.ALLAY, tag);
+        var anotherSoulWithHealth = new Soul(EntityType.SKELETON, tag);
+
+        var filter = new EnderSoulFilter(NonNullList.of(Soul.EMPTY, soulWithHealth, anotherSoulWithHealth), true, true);
+
+        Assertions.assertFalse(filter.test(soulWithHealth));
+        Assertions.assertTrue(filter.test(Soul.of(EntityType.ALLAY)));
+        Assertions.assertFalse(filter.test(anotherSoulWithHealth));
+        Assertions.assertTrue(filter.test(Soul.of(EntityType.SKELETON)));
+        Assertions.assertTrue(filter.test(Soul.of(EntityType.COD)));
+        Assertions.assertTrue(filter.test(Soul.of(EntityType.SHEEP)));
     }
 }
