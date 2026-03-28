5b212f51311ceb352de82c5f777f585b4e594c48
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2026-03-28 22:47:23 +0000

    chore: Render neighbors transparently

--- enderio/src/main/java/com/enderio/enderio/client/foundation/widgets/ioconfig/IOConfigSceneRenderer.java
+++ enderio/src/main/java/com/enderio/enderio/client/foundation/widgets/ioconfig/IOConfigSceneRenderer.java
@@ -2,14 +2,24 @@ package com.enderio.enderio.client.foundation.widgets.ioconfig;
 
 import com.enderio.enderio.EnderIO;
 import com.enderio.enderio.client.foundation.model.ModelRenderUtil;
+import com.enderio.enderio.config.machines.MachinesConfig;
+import com.mojang.blaze3d.pipeline.BlendFunction;
+import com.mojang.blaze3d.pipeline.ColorTargetState;
+import com.mojang.blaze3d.pipeline.DepthStencilState;
+import com.mojang.blaze3d.platform.CompareOp;
 import com.mojang.blaze3d.platform.Lighting;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.ByteBufferBuilder;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.render.pip.PictureInPictureRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.SubmitNodeStorage;
 import net.minecraft.client.renderer.feature.FeatureRenderDispatcher;
+import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
@@ -17,20 +27,71 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.data.AtlasIds;
 import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.util.ARGB;
 import net.minecraft.util.LightCoordsUtil;
 import net.minecraft.world.phys.Vec3;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.model.pipeline.VertexConsumerWrapper;
+import net.neoforged.neoforge.client.pipeline.PipelineModifier;
+import net.neoforged.neoforge.client.pipeline.RegisterPipelineModifiersEvent;
 import org.joml.Vector3f;
 
+import java.util.SequencedMap;
+
+@EventBusSubscriber
 public class IOConfigSceneRenderer extends PictureInPictureRenderer<IOConfigSceneRenderState> {
+
+    public static final ResourceKey<PipelineModifier> FORCE_TRANSLUCENT = ResourceKey.create(PipelineModifier.MODIFIERS_KEY, EnderIO.id("force_translucent"));
+
+    @SubscribeEvent
+    public static void onRegisterModifiers(RegisterPipelineModifiersEvent event)
+    {
+        event.register(FORCE_TRANSLUCENT, (pipeline, name) ->
+        {
+            if (pipeline == RenderPipelines.ENTITY_CUTOUT || pipeline == RenderPipelines.ENTITY_CUTOUT_CULL || pipeline == RenderPipelines.ENTITY_SOLID)
+            {
+                return pipeline.toBuilder()
+                    .withLocation(name)
+                    .withColorTargetState(new ColorTargetState(BlendFunction.TRANSLUCENT))
+                    .build();
+            }
+            return pipeline;
+        });
+    }
+
     private static final Identifier SELECTED_ICON = EnderIO.id("block/overlay/selected_face");
 
-    private final FeatureRenderDispatcher featureRenderDispatcher;
-    private final SubmitNodeCollector collector;
+    private final FeatureRenderDispatcher solidFeatureRenderDispatcher;
+    private final SubmitNodeCollector solidCollector;
+
+    private final GhostBufferSource ghostBufferSource;
+    private final FeatureRenderDispatcher ghostFeatureRenderDispatcher;
+    private final SubmitNodeCollector ghostCollector;
 
     public IOConfigSceneRenderer(MultiBufferSource.BufferSource bufferSource) {
         super(bufferSource);
-        this.featureRenderDispatcher = Minecraft.getInstance().gameRenderer.getFeatureRenderDispatcher();
-        this.collector = featureRenderDispatcher.getSubmitNodeStorage();
+
+        var minecraft = Minecraft.getInstance();
+
+        this.solidFeatureRenderDispatcher = Minecraft.getInstance().gameRenderer.getFeatureRenderDispatcher();
+        this.solidCollector = solidFeatureRenderDispatcher.getSubmitNodeStorage();
+
+        ghostBufferSource = new GhostBufferSource(bufferSource);
+
+        ghostFeatureRenderDispatcher = new FeatureRenderDispatcher(
+            new SubmitNodeStorage(),
+            minecraft.getModelManager(),
+            ghostBufferSource,
+            minecraft.getAtlasManager(),
+            minecraft.renderBuffers().outlineBufferSource(),
+            minecraft.renderBuffers().crumblingBufferSource(),
+            minecraft.font,
+            minecraft.gameRenderer.getGameRenderState()
+        );
+
+        ghostCollector = ghostFeatureRenderDispatcher.getSubmitNodeStorage();
     }
 
     @Override
@@ -49,20 +110,30 @@ public class IOConfigSceneRenderer extends PictureInPictureRenderer<IOConfigScen
 
         poseStack.mulPose(state.viewMatrix());
 
-        Minecraft.getInstance().gameRenderer.getLighting().setupFor(Lighting.Entry.LEVEL);
+        Minecraft.getInstance().gameRenderer.getLighting().setupFor(Lighting.Entry.ITEMS_FLAT);
+
+        RenderSystem.pushPipelineModifier(FORCE_TRANSLUCENT);
 
         if (state.shouldRenderNeighbors()) {
             for (var block : state.neighborBlocks()) {
-                // TODO: Render translucent...
-                renderBlock(poseStack,
-                    block);
+                renderBlock(poseStack, ghostCollector, block);
             }
         }
 
+        ghostFeatureRenderDispatcher.renderAllFeatures();
+        ghostBufferSource.endBatch();
+
+        RenderSystem.popPipelineModifier();
+
+        var device = RenderSystem.getDevice();
+        device.createCommandEncoder().clearDepthTexture(this.depthTexture, 1f);
+
         for (var block : state.primaryBlocks()) {
-            renderBlock(poseStack,
-                block);
+            renderBlock(poseStack, solidCollector, block);
         }
+
+        solidFeatureRenderDispatcher.renderAllFeatures();
+        bufferSource.endBatch();
 
         if (state.selection() != null) {
             renderSelection(poseStack, state.selection().getFirst(), state.selection().getSecond());
@@ -71,14 +142,11 @@ public class IOConfigSceneRenderer extends PictureInPictureRenderer<IOConfigScen
         poseStack.popPose();
     }
 
-    private void renderBlock(PoseStack poseStack, IOConfigSceneBlock block) {
+    private void renderBlock(PoseStack poseStack, SubmitNodeCollector submitNodeCollector, IOConfigSceneBlock block) {
         poseStack.pushPose();
         poseStack.translate(new Vec3(block.pos()));
 
-        block.blockModelRenderState().submit(poseStack, collector, LightCoordsUtil.FULL_BRIGHT, OverlayTexture.NO_OVERLAY, 0);
-
-        featureRenderDispatcher.renderAllFeatures();
-        bufferSource.endBatch();
+        block.blockModelRenderState().submit(poseStack, submitNodeCollector, LightCoordsUtil.FULL_BRIGHT, OverlayTexture.NO_OVERLAY, 0);
 
         poseStack.popPose();
     }
@@ -115,5 +183,42 @@ public class IOConfigSceneRenderer extends PictureInPictureRenderer<IOConfigScen
     @Override
     protected String getTextureLabel() {
         return "EnderIO IO Config Overlay";
+    }
+
+    // Transparency hack
+
+    private static class GhostBufferSource extends MultiBufferSource.BufferSource {
+
+        public GhostBufferSource(MultiBufferSource.BufferSource bufferSource) {
+            super(bufferSource.sharedBuffer, bufferSource.fixedBuffers);
+        }
+
+        protected GhostBufferSource(ByteBufferBuilder sharedBuffer, SequencedMap<RenderType, ByteBufferBuilder> fixedBuffers) {
+            super(sharedBuffer, fixedBuffers);
+        }
+
+        @Override
+        public VertexConsumer getBuffer(RenderType renderType) {
+            VertexConsumer consumer = super.getBuffer(renderType);
+            return new AlphaWrapper(consumer);
+        }
+
+        private static class AlphaWrapper extends VertexConsumerWrapper {
+            public AlphaWrapper(VertexConsumer vertexConsumer) {
+                super(vertexConsumer);
+            }
+
+            @Override
+            public VertexConsumer setColor(int r, int g, int b, int a) {
+                super.setColor(r, g, b, /*MachinesConfig.CLIENT.IO_CONFIG_NEIGHBOUR_TRANSPARENCY.get().floatValue()*/ 0.7f);
+                return this;
+            }
+
+            @Override
+            public VertexConsumer setColor(int packedColor) {
+                super.setColor(ARGB.color(/*MachinesConfig.CLIENT.IO_CONFIG_NEIGHBOUR_TRANSPARENCY.get().floatValue()*/ 0.7f, packedColor));
+                return this;
+            }
+        }
     }
 }
