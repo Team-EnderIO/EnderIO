6609dcfd7c74520e5cd141d97b0462eef64ea59e
tterrag <tterrag1098@gmail.com>
2026-04-24 20:54:00 -0400

    chore: upgrade foojay resolver and fix run config name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,7 +30,7 @@ pluginManagement {
 rootProject.name = "EnderIO"
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
     id("net.neoforged.moddev.repositories")
 }
 
