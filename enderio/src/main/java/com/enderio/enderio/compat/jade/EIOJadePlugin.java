c81b0e104629740267cfc0e0fe79fc0cffdacd7c
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2026-05-06 21:52:30 +0100

    chore: Clean and clarify getCloneItemStack logic

--- enderio/src/main/java/com/enderio/enderio/compat/jade/EIOJadePlugin.java
+++ enderio/src/main/java/com/enderio/enderio/compat/jade/EIOJadePlugin.java
@@ -1,32 +1,37 @@
 package com.enderio.enderio.compat.jade;
 
+import com.enderio.enderio.EnderIO;
 import com.enderio.enderio.api.conduits.bundle.ConduitBundle;
 import com.enderio.enderio.client.content.conduits.model.facades.FacadeUtil;
 import com.enderio.enderio.init.EIOBlocks;
+import net.minecraft.resources.ResourceLocation;
 import snownee.jade.api.BlockAccessor;
 import snownee.jade.api.IWailaClientRegistration;
 import snownee.jade.api.IWailaPlugin;
 import snownee.jade.api.WailaPlugin;
 
 @WailaPlugin
-public class EIOConduitsJadePlugin implements IWailaPlugin {
+public class EIOJadePlugin implements IWailaPlugin {
+
+    public static final ResourceLocation SOUL_BOUND_COMPONENT = EnderIO.rl("soul_bound");
 
     // TODO: Could implement stuff like a waila tooltip for bound souls.
 
     @Override
     public void registerClient(IWailaClientRegistration registration) {
-        // Show the correct conduit (or facade item)
+        // Use clone item stack to work out which item to show.
         registration.usePickedResult(EIOBlocks.CONDUIT_BUNDLE.get());
 
-        // Completely replace the block accessor with the facade block if it exists
+        // Completely replace the block accessor with the facade block if it exists.
+        // This prevents any special widgets (such as exposed capabilities like energy) from appearing when covered by a facade.
         registration.addRayTraceCallback((hitResult, accessor, originalAccessor) -> {
             if (accessor instanceof BlockAccessor blockAccessor) {
                 if (blockAccessor.getBlockEntity() instanceof ConduitBundle conduitBundle && conduitBundle.hasFacade()
-                        && FacadeUtil.areFacadesVisible(blockAccessor.getPlayer())) {
+                    && FacadeUtil.areFacadesVisible(blockAccessor.getPlayer())) {
                     return registration.blockAccessor()
-                            .from(blockAccessor)
-                            .fakeBlock(conduitBundle.getFacadeBlock().asItem().getDefaultInstance())
-                            .build();
+                        .from(blockAccessor)
+                        .fakeBlock(conduitBundle.getFacadeBlock().asItem().getDefaultInstance())
+                        .build();
                 }
             }
             return accessor;
