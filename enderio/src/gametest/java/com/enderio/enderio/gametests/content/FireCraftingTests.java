8d3f0b04fbb3b2c0d76e0d21a56a9300eb79b08f
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2026-06-28 16:02:17 +0200

    feat: add gametests

--- enderio/src/gametest/java/com/enderio/enderio/gametests/content/FireCraftingTests.java
+++ enderio/src/gametest/java/com/enderio/enderio/gametests/content/FireCraftingTests.java
@@ -1,7 +1,9 @@
 package com.enderio.enderio.gametests.content;
 
 import com.enderio.enderio.config.base.BaseConfig;
+import com.enderio.enderio.content.cold_fire.ColdFireBlock;
 import com.enderio.enderio.gametests.util.EnderGameTestHelper;
+import com.enderio.enderio.init.EIOBlocks;
 import com.enderio.enderio.init.EIOFluids;
 import com.enderio.enderio.init.EIOItems;
 import net.minecraft.core.BlockPos;
@@ -32,6 +34,22 @@ public class FireCraftingTests {
                     helper.assertBlockPresent(Blocks.GRANITE, BASE_POS);
                 })
                 .thenSucceed());
+    }
+
+    @GameTest(timeoutTicks = 300)
+    @TestHolder(description = "Tests that cold fire on a valid base block produces item drops.")
+    public static void testColdFireBlockCraftingDrops(final DynamicTest test) {
+        test.registerGameTestTemplate(() -> StructureTemplateBuilder.withSize(1, 2, 1)
+            .set(0, 0, 0, Blocks.GRANITE.defaultBlockState()));
+
+        test.onGameTest(EnderGameTestHelper.class, helper -> helper.startSequence()
+            .thenExecute(() -> helper.setBlock(FIRE_POS, EIOBlocks.COLD_FIRE.get().defaultBlockState()))
+            .thenExecuteAfter(BaseConfig.COMMON.INFINITY.FIRE_MIN_AGE.get() + 1, () -> helper.setBlock(FIRE_POS, Blocks.AIR.defaultBlockState()))
+            .thenExecute(() -> {
+                helper.assertItemEntityPresent(EIOItems.GRAINS_OF_INFINITY.get(), FIRE_POS, 2.0);
+                helper.assertBlockPresent(Blocks.GRANITE, BASE_POS);
+            })
+            .thenSucceed());
     }
 
     @GameTest
@@ -80,5 +98,18 @@ public class FireCraftingTests {
                     helper.assertBlockPresent(Blocks.STONE, BASE_POS);
                 })
                 .thenSucceed());
+    }
+
+    @GameTest(timeoutTicks = 310)
+    @TestHolder(description = "Tests that cold fire goes out.")
+    public static void testColdFireAges(final DynamicTest test) {
+        test.registerGameTestTemplate(() -> StructureTemplateBuilder.withSize(1, 2, 1)
+            .set(0, 0, 0, Blocks.GRANITE.defaultBlockState()));
+
+        test.onGameTest(EnderGameTestHelper.class, helper -> helper.startSequence()
+            .thenExecute(() -> helper.setBlock(FIRE_POS,EIOBlocks.COLD_FIRE.get().defaultBlockState().setValue(ColdFireBlock.AGE, 15)))
+            .thenIdle(300)
+            .thenExecute(() -> helper.assertBlockPresent(Blocks.AIR, FIRE_POS))
+            .thenSucceed());
     }
 }
